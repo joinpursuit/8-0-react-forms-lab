fc9842ca240306bc35f8c91cc85b5f721dcd4e99
JoseBorbon <joserborbon@gmail.com>
2021-11-05 11:56:31 -0400

    add constructor and method to pass to form comp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
-import Form from "./Form";
-import "./App.css";
+import React from 'react';
+import Form from './Form';
+import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { result: '' };
+    this.getResult = this.getResult.bind(this);
+  }
+
+  getResult(input) {
+    this.setState({ result: input });
+  }
+
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form getResult={this.getResult} />
         <section id="result">
-          <p></p>
+          <p>{this.state.result}</p>
         </section>
       </main>
     );
