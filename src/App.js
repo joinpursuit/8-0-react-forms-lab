f6e252e2137ad27de52665f6752d23365d8955f9
salrey <tekdehn@gmail.com>
2021-11-05 13:06:53 -0400

    Display error message for an invalid input

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import Form from "./Form";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      result: ""
+    }
+  }
+
+  handleInput = (event) => {
+    event.preventDefault();
+
+    if (event.target.value === "") {
+      this.setState({
+        result: "Invalid input."
+      })
+    }
+  }
+
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form handleInput={this.handleInput} result={this.state.result}/>
         <section id="result">
-          <p></p>
+          <p>{this.state.result}</p>
         </section>
       </main>
     );
--- src/Form.js
+++ src/Form.js
@@ -2,17 +2,18 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
+  
   render() {
     return (
       <form>
-        <input id="values" name="values" type="text" />
+        <input id="values" name="values" type="text" value={this.props.result}/>
         <select id="operation" name="operation">
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
           <option value="mode">mode</option>
         </select>
-        <button type="submit">Calculate</button>
+        <button onClick={this.props.handleInput} type="submit">Calculate</button>
       </form>
     );
   }
