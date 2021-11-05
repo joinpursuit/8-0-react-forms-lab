3b278a5805b25573bf7e89349917427990401f5e
JoseBorbon <joserborbon@gmail.com>
2021-11-05 11:57:00 -0400

    add conditional logic, methods, + event listeners

--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,86 @@
-import React from "react";
-import "./Form.css";
+import React from 'react';
+import './Form.css';
 
-class Form extends React.Component {
+export default class Form extends React.Component {
+  static defaultProps = {
+    ops: {
+      sum: (arr) => arr.reduce((acc, cVal) => acc + Number(cVal), 0),
+      average: (arr) =>
+        arr.reduce((acc, cVal) => acc + Number(cVal), 0) / arr.length,
+      mode: (arr) => {
+        const hashMap = arr.reduce((acc, cVal) => {
+          acc[cVal] = (acc[cVal] || 0) + 1;
+          return acc;
+        }, {});
+        let mode;
+        let highestCount = -Infinity;
+
+        for (const prop in hashMap) {
+          //if current map at key is greater than highestCount
+          if (hashMap[prop] > highestCount) {
+            mode = prop;
+            highestCount = hashMap[prop];
+          }
+        }
+        return mode;
+      },
+    },
+  };
+  constructor(props) {
+    super(props);
+    this.state = { operation: null, input: '' };
+    this.changeOperation = this.changeOperation.bind(this);
+    this.changeInput = this.changeInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    const regex = /^[0-9]+(,[0-9]+)*$/;
+    if (!regex.test(this.state.input)) {
+      this.props.getResult('Invalid input.');
+      return;
+    }
+
+    const result = this.props.ops[this.state.operation](
+      this.state.input.split(',')
+    );
+    this.props.getResult(result);
+  }
+
+  changeOperation(evt) {
+    this.setState(() => {
+      return { [evt.target.name]: evt.target.value };
+    });
+  }
+
+  changeInput(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
   render() {
     return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={this.handleSubmit}>
+        <input
+          id="values"
+          name="input"
+          type="text"
+          onChange={this.changeInput}
+          value={this.state.input}
+        />
+        <select id="operation" name="operation" onChange={this.changeOperation}>
           <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
+          <option value="sum" name="sum">
+            sum
+          </option>
+          <option value="average" name="sum">
+            average
+          </option>
+          <option value="mode" name="sum">
+            mode
+          </option>
         </select>
         <button type="submit">Calculate</button>
       </form>
     );
   }
 }
-
-export default Form;
