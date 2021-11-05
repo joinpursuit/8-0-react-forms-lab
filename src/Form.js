e112193360982313088073506e25074d96577298
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-11-05 15:15:51 -0400

    adds preventDefault

--- src/Form.js
+++ src/Form.js
@@ -2,10 +2,26 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userInput: "",
+      feature: "sum",
+    };
+  }
+
+  preventDefault = (event) => {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form>
-        <input id="values" name="values" type="text" />
+      <form onSubmit={this.preventDefault}>
+        <input 
+          id="values" 
+          name="values" 
+          type="text"
+        />
         <select id="operation" name="operation">
           <option value=""></option>
           <option value="sum">sum</option>
