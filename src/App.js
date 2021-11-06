7c2c4e9fd35737d5681e37d86447cb9a35bdfc6a
justin <justin.d.diaz@gmail.com>
2021-11-06 13:59:21 -0400

    first test done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,67 @@ import Form from "./Form";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      result: "",
+    };
+  }
+  calculateButton = (userInput, dropdown) => {
+    if (userInput === "" || dropdown === "") {
+      this.setState({
+        result: "Invalid input.",
+      });
+    } else {
+      let inputArray = userInput.split(",").map(Number);
+      if (dropdown === "sum") {
+        const sum = inputArray.reduce(
+          (previousValue, currentValue) => previousValue + currentValue,
+          0
+        );
+        this.setState({
+          result: sum,
+        });
+      } else if (dropdown === "average") {
+        const average =
+          inputArray.reduce(
+            (previousValue, currentValue) => previousValue + currentValue,
+            0
+          ) / inputArray.length;
+        this.setState({
+          result: average,
+        });
+      } else {
+        const dropdownMode = {};
+        for (let arr of inputArray) {
+          if (dropdownMode[arr] !== undefined) {
+            dropdownMode[arr] = dropdownMode[arr] + 1;
+          } else {
+            dropdownMode[arr] = 1;
+          }
+        }
+        let result = 0;
+        let max = -Infinity;
+        for (let key in dropdownMode) {
+          if (dropdownMode[key] > max) {
+            max = dropdownMode[key];
+            result = key;
+          }
+        }
+        this.setState({
+          result: result,
+        });
+      }
+    }
+  };
+
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form calculateButton={this.calculateButton} />
         <section id="result">
-          <p></p>
+          <p>{this.state.result}</p>
         </section>
       </main>
     );
--- src/Form.js
+++ src/Form.js
@@ -2,17 +2,47 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userInput: "",
+      result: "",
+      dropdown: "",
+    };
+  }
+  handleButtonClick = (event) => {
+    event.preventDefault();
+    this.props.calculateButton(this.state.userInput, this.state.dropdown);
+  };
+  handleInputChange = (event) => {
+    this.setState({
+      userInput: event.target.value,
+    });
+  };
+  handleDropdown = (event) => {
+    this.setState({
+      dropdown: event.target.value,
+    });
+  };
   render() {
     return (
       <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+        <input
+          id="values"
+          name="values"
+          type="text"
+          value={this.state.userInput}
+          onChange={this.handleInputChange}
+        />
+        <select id="operation" name="operation" onChange={this.handleDropdown}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
           <option value="mode">mode</option>
         </select>
-        <button type="submit">Calculate</button>
+        <button onClick={this.handleButtonClick} type="submit">
+          Calculate
+        </button>
       </form>
     );
   }
