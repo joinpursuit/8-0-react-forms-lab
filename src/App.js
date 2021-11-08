030e783a865a1ab8078e6d833e38c729fabcf6c7
Jalamang <Jalamangjanka@gmail.com>
2021-11-07 23:29:49 -0500

    Added  components and logic to play the game

--- src/App.js
+++ src/App.js
@@ -3,13 +3,65 @@ import Form from "./Form";
 import "./App.css";
 
 class App extends React.Component {
+constructor(){
+  super();
+  this.state = {
+    result: '',
+  }
+}
+
+getResult = (userInput, operation) =>{
+   const number = userInput.split(',').map(str => (Number(str)))
+   console.log(Number.isNaN(userInput))
+  if(userInput === "" || operation === "" || isNaN(userInput)){
+   
+this.setState({
+  result: 'Invalid input.'
+})
+  } else if(operation === "sum"){
+  const  sum = (arr) =>arr.reduce((previousValue, currentValue) => previousValue + Number(currentValue),0);
+this.setState({
+  result:sum(number),
+})
+  } else if(operation=== 'average'){
+   const average = (arr) =>arr.reduce((previousValue, currentValue) => previousValue + Number(currentValue),0) / arr.length;
+  this.setState({
+    result: (average(number)).toFixed(2)
+  })
+  } else {
+const mode = (arr) => {
+        const hashMap = {};
+        for (let el of arr) {
+          if (hashMap[el] !== undefined) {
+            hashMap[el] = hashMap[el] + 1;
+          } else {
+            hashMap[el] = 1;
+          }
+        }
+        let result = 0;
+        let max = -Infinity;
+        for (let key in hashMap) {
+          if (hashMap[key] > max) {
+            max = hashMap[key];
+            result = key;
+          }
+        }
+        return result;
+      }
+      this.setState({
+        result:mode(number)
+      })
+  }
+    
+    }
+
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form getResult={this.getResult}/>
         <section id="result">
-          <p></p>
+          <p> {this.state.result}</p>
         </section>
       </main>
     );
--- src/Form.js
+++ src/Form.js
@@ -2,17 +2,52 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
+    constructor() {
+    super();
+    this.state = {
+      operation: "",
+      userInput: "",
+    };
+  }
+
+  // //userInput function
+  handleUserInput = (event) => {
+    this.setState({userInput: event.target.value});
+  };
+
+  handleModeChange = (event) => {
+      this.setState({operation: event.target.value,})
+  }
+
+  //form submit
+  handleSubmit = (event) => {
+    event.preventDefault();
+         this.props.getResult(this.state.userInput, this.state.operation) 
+  };
+
+
   render() {
     return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form >
+        <input
+          id="values"
+          name="values"
+          type="text"
+          onChange={this.handleUserInput}
+          value={this.state.userInput}
+        />
+        <select
+          id="operation"
+          name="operation"
+          onChange={this.handleModeChange}
+          
+        >
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
           <option value="mode">mode</option>
         </select>
-        <button type="submit">Calculate</button>
+        <button onClick={this.handleSubmit} type="submit" >Calculate</button>
       </form>
     );
   }
