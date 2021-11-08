217ff8969d913da0162d8d92a587adbba172f368
Younes Tahiri <you@example.com>
2021-11-08 14:07:56 -0500

    completed the react forms lab

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import Form from "./Form";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      result: "",
+      error: "",
+    };
+  }
+
+  afterSubmit = (submited, error) => {
+    this.setState({
+      result: submited,
+      error: error,
+    });
+  };
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form afterSubmit={this.afterSubmit} />
         <section id="result">
-          <p></p>
+          <p>{this.state.result}</p>
+          <p>{this.state.error}</p>
         </section>
       </main>
     );
@@ -17,3 +32,15 @@ class App extends React.Component {
 }
 
 export default App;
+
+// getUserInput={this.getUserInput} getSelection={this.getSelection}
+// getUserInput = (input) => {
+//   this.setState({
+//     userInput: input,
+//   });
+// };
+// getSelection = (selection) => {
+//   this.setState({
+//     operator: selection,
+//   });
+// };
