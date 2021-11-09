cb866144828c132fb1509537cd7060d511b27208
andres ballares <andr3sb@hotmail.com>
2021-11-08 22:12:52 -0500

    submitting lab

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from "react";
 import Form from "./Form";
 import "./App.css";
-
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      result: "",
+      error: "",
+    };
+  }
+  afterSubmit = (submited, error) => {
+    this.setState({
+      result: submited,
+      error: error,
+    });
+  };
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form afterSubmit={this.afterSubmit} />
         <section id="result">
-          <p></p>
+          <p>{this.state.result}</p>
+          <p>{this.state.error}</p>
         </section>
       </main>
     );
