44c7b9f28b9d8d39e37149c99c9ae5bb18472198
Fatema Kabir <fatema.kabir@pursuit.org>
2021-11-09 00:21:36 -0500

    react-froms-lab

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import Form from "./Form";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      result: "",
+    };
+  }
+
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form app={this} />
         <section id="result">
-          <p></p>
+          <p>{this.state.result}</p>
         </section>
       </main>
     );
@@ -17,3 +24,51 @@ class App extends React.Component {
 }
 
 export default App;
+
+/* STATE: what state do we need?
+ * 1. the result - "Invalid input." || Number/sum/average/mode
+ * 2. the operation - which operation are we doing?
+ * 3. the values - which values are we working with? (contents of text input)
+ */
+
+/*
+where do we put these?
+the result - should live in App because it needs to put that state in result
+
+
+*/
+
+/*
+State - what State do we NEED
+1. the result - "invalid input" || the result of sum/average/mode
+-- should live in App because it needs to put that state in #result
+2. the operation - which operation are we doing?
+3. the values - which values are we working with? - aka the contents of the text input
+-- should live in the form
+*/
+
+//*Methods - what things do we need to DO
+
+//*Form:
+//handles submission
+//-prevent default behavior
+//-send the data somewhere ... to App
+//-might look for invalid input
+//-might transform data into calculable form
+//needs a handler for the text input
+//needs a handler for the dropdown
+
+//*App:
+//-needs a calculated method or several
+//-Sum
+//-Average
+//-Mode
+
+//userInput has to be a string of numbers
+//convert the userInput to an array
+//perform the operation on the userInput
+//depending on the operation we have to have methods for sum, mode, average
+//operations are handle with onClick
+//if userInput is invalid return error message "invalid input"
+//result is handled with onSubmit
+//return the result
--- src/Form.js
+++ src/Form.js
@@ -2,17 +2,91 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      operation: "",
+      userInput: "",
+    };
+  }
+  // event listener for userInput
+  // change our state to reflect what the user has typed in
+  handleUserInput = (event) => {
+    this.setState({ userInput: event.target.value });
+  }; // handles the dropdown change (operation to perform)
+
+  handleOperation = (event) => {
+    this.setState({ operation: event.target.value });
+  };
+
+  // on submit depending on the userInput and the dropdown option selected performs the operation and displays result
+  // on submit if userInput is invalid displays "Invalid input" in result section.
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (
+      this.state.userInput.length === 0 ||
+      !Number(this.state.userInput.split(",").join(""))
+    ) {
+      this.props.app.setState({ result: "Invalid input." });
+    }
+    // userInput has to be a number or else it is invalid
+    // userInput can not be empty
+    else {
+      if (this.state.operation === "sum") {
+        const sum = this.state.userInput.split(",").reduce((a, b) => {
+          return Number(a) + Number(b);
+        });
+        this.props.app.setState({ result: sum });
+      }
+
+      if (this.state.operation === "average") {
+        const average =
+          this.state.userInput.split(",").reduce((a, b) => {
+            return Number(a) + Number(b);
+          }) / this.state.userInput.split(",").length;
+        this.props.app.setState({ result: average });
+      }
+
+      if (this.state.operation === "mode") {
+        const obj = {};
+        for (const element of this.state.userInput.split(",")) {
+          if (obj[element]) {
+            obj[element] = ++obj[element];
+          } else {
+            obj[element] = 1;
+          }
+        }
+        let mode;
+        for (const key in obj) {
+          if (obj[key] > obj[mode] || !obj[mode]) {
+            mode = key;
+          }
+        }
+        this.props.app.setState({ result: mode });
+      }
+    }
+  };
+
   render() {
     return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
-          <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
+      <form onSubmit={this.handleSubmit}>
+              
+        <input
+          id="values"
+          name="values"
+          type="text"
+          onChange={this.handleUserInput}
+        />
+                
+        <select onChange={this.handleOperation} id="operation" name="operation">
+                    <option value=""></option>
+                    <option value="sum">sum</option>
+                    <option value="average">average</option>
+                    <option value="mode">mode</option>
+                  
         </select>
-        <button type="submit">Calculate</button>
+                <button type="submit">Calculate</button>
+              
       </form>
     );
   }
