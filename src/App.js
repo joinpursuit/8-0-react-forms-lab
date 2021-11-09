cbbda1426a47a3edf904e30c6dbc0f35e1ffec12
wal3sca <walescaambroise@gmail.com>
2021-11-08 23:24:50 -0500

    function dropdown in form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from "react";
 import Form from "./Form";
 import "./App.css";
 
+// to change userInput you can use setState OR use this.setState.userInput.value
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      result: "",
+    };
+  }
+
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form fun={this} />
         <section id="result">
-          <p></p>
+          <p>{this.state.result}</p>
         </section>
       </main>
     );
@@ -17,3 +25,16 @@ class App extends React.Component {
 }
 
 export default App;
+
+/* STATE: what state do we need?
+ * 1. the result - "Invalid input." || Number/sum/average/mode
+ * 2. the operation - which operation are we doing?
+ * 3. the values - which values are we working with? (contents of text input)
+ */
+
+/*
+where do we put these?
+the result - should live in App because it needs to put that state in result
+
+
+*/
--- src/Form.js
+++ src/Form.js
@@ -2,11 +2,82 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectChange: "",
+      userInput: "",
+    };
+  }
+  // event listener for userInput
+  handleUserInput = (event) => {
+    this.setState({ userInput: event.target.value });
+  };
+
+  // select change function method
+  selectChange = (event) => {
+    this.setState({ selectChange: event.target.value });
+  };
+
+  // stop reload
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (
+      this.state.userInput.length === 0 ||
+      !Number(this.state.userInput.split(",").join(""))
+    ) {
+      this.props.fun.setState({ result: "Invalid input." });
+    }
+
+    // function for dropDown
+    else {
+      if (this.state.selectChange === "sum") {
+        const sum = this.state.userInput.split(",").reduce((a, b) => {
+          return Number(a) + Number(b);
+        });
+        this.props.fun.setState({ result: sum });
+      }
+
+      if (this.state.selectChange === "average") {
+        const average =
+          this.state.userInput.split(",").reduce((a, b) => {
+            return Number(a) + Number(b);
+          }) / this.state.userInput.split(",").length;
+        this.props.fun.setState({ result: average });
+      }
+
+      if (this.state.selectChange === "mode") {
+        const obj = {};
+        for (const element of this.state.userInput.split(",")) {
+          if (obj[element]) {
+            obj[element] = ++obj[element];
+          } else {
+            obj[element] = 1;
+          }
+        }
+        let mode;
+        for (const key in obj) {
+          if (obj[key] > obj[mode] || !obj[mode]) {
+            mode = key;
+          }
+        }
+        this.props.fun.setState({ result: mode });
+      }
+    }
+  };
+
+  // onSubmit allows access to
   render() {
     return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={this.handleSubmit}>
+        <input
+          id="values"
+          name="values"
+          type="text"
+          onChange={this.handleUserInput}
+        />
+
+        <select onChange={this.selectChange} id="operation" name="operation">
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
