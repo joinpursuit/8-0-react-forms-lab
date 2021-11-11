caa71d2495052a5822505ca1c95c6a6756b2aeaa
Name Ping Louie <namepinglouie@gmail.com>
2021-11-11 10:56:51 -0500

    app.js keep handleNumbers comments

--- src/App.js
+++ src/App.js
@@ -3,13 +3,85 @@ import Form from "./Form";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      operator: "",
+      numStr: "",
+      result: 0
+    }
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    this.handleCalculation();
+  }
+
+  handleOperation = (e) => {
+    this.setState({
+      operator: e.target.value,
+    })
+  }
+
+  handleNumbers = (e) => {
+    this.setState({
+      numStr: e.target.value,
+    })
+
+    // let nums = e.target.value;
+    // console.log(nums);
+    // let numsSplit = nums.split(",");
+    // let arr = [];
+    // for(let num of numsSplit) {
+    //   if(num !== "") {
+    //     arr.push(Number(num));
+    //   }
+    // }
+    // this.setState({
+    //   numArr: arr,
+    // })
+    // console.log(this.state.numArr);
+    // difference in last element with/without "," why?;
+    // console.log(numsSplit); //shows last num w/o ",";
+    // console.log(this.state.numArr); // doesn't show last num, need "," to show last num;
+
+    
+  }
+
+  handleSum = (numbers) => {
+    let total = 0;
+    for(let num of numbers) {
+      total += num;
+    }
+    return total;
+  }
+
+  handleCalculation = () => {
+    let numArr = this.state.numStr.split(",").map((num)=> {
+      return Number(num);  
+    });
+    console.log(numArr);
+
+    if(this.state.operator === "sum") {
+      console.log("trigger")
+      this.setState({
+        result: this.handleSum(numArr)
+      })
+    }
+  }
+
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form handleFormSubmit={this.handleFormSubmit} 
+              handleOperation = {this.handleOperation} 
+              operator = {this.state.operator}
+              numbersArr = {this.state.numbersArr} 
+              handleNumbers = {this.handleNumbers} 
+              />
         <section id="result">
-          <p></p>
+          <p>{this.state.result}</p>
         </section>
       </main>
     );
--- src/Form.js
+++ src/Form.js
@@ -3,10 +3,11 @@ import "./Form.css";
 
 class Form extends React.Component {
   render() {
+    const {numbersArr, operator, handleNumbers, handleOperation, handleFormSubmit} = this.props;
     return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit = {handleFormSubmit}>
+        <input id="values" name="values" type="text" value = {numbersArr} onChange = {handleNumbers} />
+        <select id="operation" name="operation" value = {operator} onChange = {handleOperation}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
