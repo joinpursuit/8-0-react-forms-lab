cd549c4aa2a681ee8d9b19bbc08062b0b5c23ef5
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-11 11:17:28 -0500

    passed all tests. no bonus features

--- src/App.js
+++ src/App.js
@@ -2,26 +2,14 @@ import React from "react";
 import Form from "./Form";
 import "./App.css";
 
-/* Hierarchy */
-//        App
-//       Form
-
 class App extends React.Component {
-  constructor(){
-    super();
-
-    this.state = {
-      result: '',
-    }
-  }
-
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form result={this.props.result}/>
+        <Form />
         <section id="result">
-          <p>Not this{this.state.result}</p>
+          <p></p>
         </section>
       </main>
     );
--- src/Form.js
+++ src/Form.js
@@ -4,88 +4,97 @@ import "./Form.css";
 class Form extends React.Component {
   constructor(){
     super();
-
     this.state = {
-      userInput: '',
+      userInput: [],
       operation: '',
-    }
-  }
-
-  handleFormSubmit=(event) => {
-    const { userInput, operation } = this.state;
-    event.preventDefault();
-    // console.log(event)
-
-    console.log("user input:", userInput)
-    console.log("the operation:", operation)
-
-    if (operation === "sum") {
-      console.log("hello sum")
-      this.setState({
-        result: 'hector result rae'
-      })
-      
-    } else if (operation === "average") {
-      console.log("find average")
-    } else if (operation === "mode") {
-      console.log("find the mode")
-    } else if (operation === "") {
-      this.setState({
-        userInput: `Invalid input.`,
-      })
+      result: '',
     }
   }
 
   handleUserInput=(event) => {
     this.setState({
-      userInput: event.target.value,
+      userInput: event.target.value.split(','),
     })
   }
 
   handleOperationChange=(event) => {
     this.setState({
       operation: event.target.value,
-      result: '',
     })
   }
 
-  /*
-    `calculatesResult` contain all possible operations:
-      - sum
-      - average
-      - mode
-    if statement that calls back operation change
-    if user picks `sum`, operation picks that `sum` result
-  */
-  calculatesResult=() => {
-    // calculate here
+  handleFormSubmit=(event) => {
+    const { userInput, operation } = this.state;
+    event.preventDefault();
+
+    if (operation === ''){
+      this.setState({
+        result: 'Invalid input.',
+      })
+    } else if (operation === "sum") {
+      this.setState({
+        result: userInput.reduce((a, b) => Number(a) + Number(b)),
+        userInput: [],
+        operation: '',
+      })
+    } else if (operation === "average") {
+      this.setState({
+        result: userInput.reduce((a, b) => Number(a) + Number(b)) / userInput.length,
+        userInput: [],
+        operation: '',
+      })
+    } else if (operation === "mode") {
+      let modeObj = {};
+      let mode = 1;
+      for (let num of userInput) {
+        if (!modeObj[num]) {
+          modeObj[num] = 1;
+        } else {
+          modeObj[num] += 1;
+        }
+      }
+
+      for (let num in modeObj) {
+        if (modeObj[num] > mode) {
+          mode = num;
+        }
+      }
+
+      this.setState({
+        result: mode,
+        userInput: [],
+        operation: '',
+      })
+    }
   }
 
   render() {
-    console.log(this.state);
-
+    const { result, userInput, operation } = this.state;
     return (
-      <form onSubmit={this.handleFormSubmit} className="form-container">
-        <input 
-          id="values"
-          name="values"
-          type="text"
-          value={this.state.userInput}
-          onChange={this.handleUserInput}
-        />
-        <select
-          id="operation"
-          name="operation"
-          value={this.state.operation}
-          onChange={this.handleOperationChange}
-        >
-          <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
-        </select>
-        <button type="submit">Calculate</button>
-      </form>
+      <div>
+        <form onSubmit={this.handleFormSubmit} className="form-container">
+          <input 
+            id="values"
+            name="values"
+            type="text"
+            value={userInput}
+            onChange={this.handleUserInput}
+          />
+          <select
+            id="operation"
+            name="operation"
+            value={operation}
+            onChange={this.handleOperationChange}
+          >
+            <option value=""></option>
+            <option value="sum">sum</option>
+            <option value="average">average</option>
+            <option value="mode">mode</option>
+          </select>
+          <button type="submit">Calculate</button>
+        </form>
+        <p id="result">{result}</p>
+      </div>
     );
   }
 }
