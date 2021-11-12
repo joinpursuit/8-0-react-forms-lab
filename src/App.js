9bae2c0ebe0132d45ff85cedb79742c37eb970c6
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2021-11-12 02:42:17 -0500

    Lab completed the proper way?

--- src/App.js
+++ src/App.js
@@ -3,14 +3,107 @@ import Form from "./Form";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      userInput: [],
+      operation: "",
+      result: "",
+    }
+  }
+
+  handleClick = (event) => {
+    const { operation, userInput } = this.state
+    event.preventDefault();
+    switch (operation) {
+      case "sum":
+        this.handleSumChange(userInput);
+        break;
+      case "average":
+        this.handleAvgChange(userInput);
+        break;
+      case "mode":
+        this.handleModeChange(userInput);
+        break;
+      default:
+        this.handleDefault();
+    }
+  }
+
+  handleUserInput = (event) => {
+    const numsInput = event.target.value.split(",").map((el) => {
+      return el;
+    })
+    this.setState({
+      userInput: numsInput,
+    });
+  }
+
+  handleOperationChange = (event) => {
+    const selectedOperator = event.target.value
+    this.setState({
+      operation: selectedOperator
+    })
+  }
+
+  handleDefault = () =>{
+    this.setState({
+      result: "Invalid input.",
+    })
+}
+
+  handleSumChange = (elArr) =>{
+    const numsInput = elArr.reduce((current, previous) => Number(current) + Number(previous))
+      this.setState({
+        result: numsInput
+      })
+  }
+
+  handleAvgChange = (elArr) =>{
+    const numsInput = elArr.reduce((current, previous) => Number(current) + Number(previous)) / elArr.length
+      this.setState({
+        result: numsInput,
+      })
+  }
+
+  handleModeChange = (elArr) =>{
+
+    let obj = {};
+    let mode = 1;
+    let number = elArr;
+    for(let num of number){
+      if(!obj[num]){
+        obj[num] = 1;
+      }else{
+        obj[num] += 1;
+      }
+    }
+
+    for(let num in obj){
+      if(obj[num] > mode){
+        mode = num;
+      }
+    }
+
+    this.setState({
+      result: mode,
+    });
+  }
 
   render() {
-    return (
+    const { userInput, operation, result } = this.state;
+    return ( 
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form  
+        userInput={userInput} 
+        handleUserInput={this.handleUserInput}
+        handleClick={this.handleClick}
+        operation={operation}
+        handleOperationChange={this.handleOperationChange}
+        />
         <section id="result">
-          <p></p>
+          <p>{result}</p>
         </section>
       </main>
     );
--- src/Form.js
+++ src/Form.js
@@ -2,83 +2,12 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      userInput: "",
-      operation: "",
-      result: "",
-    }
-  }
-
-  handleUserInput = (event) => {
-    const numsInput = event.target.value.split(",")
-    this.setState({
-      userInput: numsInput,
-    });
-  }
-
-  handleOperationChange = (event) => {
-    this.setState({
-      operation: event.target.value
-    })
-  }
-
-  handleFormSubmit = (event) => {
-    const { userInput, operation } = this.state;
-    event.preventDefault();
-    if(operation === ""){
-      this.setState({
-        result: "Invalid input.",
-      })
-    }else if(operation === "sum"){
-      this.setState({
-        result: userInput.reduce((current, previous) => Number(current) + Number(previous)),
-        userInput: [],
-        operation: "",
-      })
-    }else if(operation === "average"){
-      this.setState({
-        result: userInput.reduce((current, previous) => Number(current) + Number(previous)) / userInput.length,
-        userInput: "",
-        operation: "",
-      })
-    }else if(operation === "mode"){
-      let obj = {};
-      let mode = 1;
-      let number = userInput;
-      console.log(number)
-      for(let num of number){
-        if(!obj[num]){
-          obj[num] = 1;
-        }else{
-          obj[num] += 1;
-        }
-      }
-
-      for(let num in obj){
-        if(obj[num] > mode){
-          mode = num;
-        }
-      }
-
-      this.setState({
-        result: mode,
-        userInput: "",
-        operation: "",
-      });
-    }
-  }
-
-  
   render() {
-    const { userInput, operation, result } = this.state;
-
+    const { userInput, handleUserInput, handleClick, operation, handleOperationChange } = this.props;
     return (
-      <div>
-        <form onSubmit={this.handleFormSubmit} className="form-container">
-          <input id="values" name="values" type="text" value={userInput} onChange={this.handleUserInput} />
-          <select id="operation" name="operation" value={operation} onChange={this.handleOperationChange}>
+        <form onSubmit={handleClick} className="form-container">
+          <input className="error" id="values" name="values" type="text" value={userInput} onChange={handleUserInput} />
+          <select className="error" id="operation" name="operation" value={operation} onChange={handleOperationChange}>
             <option value=""></option>
             <option value="sum">sum</option>
             <option value="average">average</option>
@@ -86,10 +15,55 @@ class Form extends React.Component {
           </select>
           <button type="submit">Calculate</button>
         </form>
-        <p id="result">{result}</p>
-      </div>
     );
   }
 }
 
 export default Form;
+
+
+// handleFormSubmit = (event) => {
+  //   const { userInput, operation } = this.state;
+  //   event.preventDefault();
+  //   if(operation === ""){
+  //     this.setState({
+  //       result: "Invalid input.",
+  //     })
+  //   }else if(operation === "sum"){
+  //     this.setState({
+  //       result: userInput.reduce((current, previous) => Number(current) + Number(previous)),
+  //       userInput: [],
+  //       operation: "",
+  //     })
+  //   }else if(operation === "average"){
+  //     this.setState({
+  //       result: userInput.reduce((current, previous) => Number(current) + Number(previous)) / userInput.length,
+  //       userInput: "",
+  //       operation: "",
+  //     })
+  //   }else if(operation === "mode"){
+  //     let obj = {};
+  //     let mode = 1;
+  //     let number = userInput;
+  //     console.log(number)
+  //     for(let num of number){
+  //       if(!obj[num]){
+  //         obj[num] = 1;
+  //       }else{
+  //         obj[num] += 1;
+  //       }
+  //     }
+
+  //     for(let num in obj){
+  //       if(obj[num] > mode){
+  //         mode = num;
+  //       }
+  //     }
+
+  //     this.setState({
+  //       result: mode,
+  //       userInput: "",
+  //       operation: "",
+  //     });
+  //   }
+  // }
