a9f5e382883e4fc31318e9f69e6eb552667c2988
“Lili <lilyhuang114classb@gmail.com>
2021-11-12 13:12:25 -0500

    Still need to fix the displayResult.

--- src/Form.js
+++ src/Form.js
@@ -53,10 +53,35 @@ class Form extends React.Component {
     return hightestKey;
   }
   
+  handleValidation = (numArr) => {
+    let flag = true;
+    if(this.state.input){
+      flag = false
+    }
+    for(let num of numArr) {
+      if(isNaN(num)) {
+        flag = false;
+      }
+    }
+    return flag;
+  }
+
   handleSubmit =(event)=>{
     event.preventDefault();
     let newArr = this.state.input.split(",");
     let numArr = newArr.map(str => Number(str));
+
+    if(this.state.operation === "") {
+      this.props.handleChangeResult("Invalid input.")
+      return;
+    }
+
+    let isValid = this.handleValidation(numArr);
+    if(!isValid) {
+      this.props.handleChangeResult("Invalid input. ")
+      return;
+    }
+
     let result = 0;
     switch (this.state.operation) {
       case "sum":
@@ -72,11 +97,14 @@ class Form extends React.Component {
       default:
         break;
     }
+    this.props.handleChangeResult(result);
   }
+
+  
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input id="values" name="values" type="text" value={this.state.input} onInput={this.handleInputBox}/>
         <select id="operation" name="operation" value={this.state.value} onChange={this.handleOperationChange}>
           <option value=""></option>
