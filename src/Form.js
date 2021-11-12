46d6350fb173f02fe235d94002dcda272c0d2a7f
stephanie lucano <stephaniemarielucano@gmail.com>
2021-11-12 11:40:47 -0500

    Says 'Invalid input.' if pressed with invalid input, calculates and displays sum, average and mode when those operations are selected.

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Form.css";
-const isInvalid = require("./isInvalid")
 
 class Form extends React.Component {
   constructor() {
     super()
     this.state = {
       userInput: "",
+      operation: "",
       errorClass: ""
     } 
   }
@@ -17,10 +17,38 @@ class Form extends React.Component {
     }) 
   }
 
+  handleOperation = (event) => {
+    this.setState({
+      operation: event.target.value
+    }) 
+  }
+
   handleSubmit = (event) => {
     event.preventDefault()
+    
+    function isInvalid (userInput) {
+      // Return true if userInput is an empty string or not a number 
+      // case 1: userInput is an "" === ""
+      // case 2: userInput is a "a, b" => ["a", "b"] => Number("a") => Number.isNaN(NaN) === true
 
-    if (isInvalid(this.state.userInput)) {
+      if (userInput === "") {
+        return true
+      } 
+
+      const isInvalid = () => {
+        userInput.split(",").some((element) => {
+          Number.isNaN(Number(element))
+        })
+      }
+      
+      if (isInvalid) {
+        return true
+      }
+    }
+      
+    const {userInput} = this.state
+
+    if (isInvalid(userInput)) {
       this.setState({
         errorClass: "error"
       })
@@ -29,6 +57,50 @@ class Form extends React.Component {
       this.setState({
         errorClass: ""
       })
+    }
+
+    if (this.state.operation === "sum") {
+      this.props.afterSubmit(
+        this.state.userInput.split(",").reduce((previousNum, currNum) => Number(previousNum) + Number(currNum))
+      );
+    }
+
+    if (this.state.operation === "average") {
+      this.props.afterSubmit(
+        this.state.userInput.split(",").reduce((previousNum, currNum) => Number(previousNum) + Number(currNum)) / this.state.userInput.split(",").length 
+      )
+    }
+
+    if (this.state.operation === "mode") {
+      
+      function mode (userInput) {
+        //Group and count elements via object, transform to array, then sort to grab the element with the highest count
+
+        const modeList = {};
+
+        userInput.split(",").forEach((element) => {
+          if (modeList[element]) {
+            modeList[element]++
+          } else {
+            modeList[element] = 1;
+          }
+        });
+        //> eg. 1,2,2,2,2,5,3 => {1:1, 2:4, 5:1, 3:1}
+        
+        const sortMode = Object.entries(modeList).sort(           (prevValue, currValue) =>  prevValue[1] - currValue[1] 
+        ); 
+        //> eg. [ [1,1], [2,4], [5,1], [3,1] ] => 
+        //      [ [1,1], [5,1], [3,1], [2,4] ]
+
+        return sortMode[sortMode.length - 1][0]; 
+        //> [2,4] => 2 is the one with the highest count
+      }
+      
+      const {userInput} = this.state
+
+      this.props.afterSubmit(
+        mode(userInput)
+      )
     }
   }
 
@@ -45,6 +117,8 @@ class Form extends React.Component {
         <select 
           id="operation" 
           name="operation"
+          onChange={this.handleOperation}
+          className={this.state.errorClass}
         >
           <option value=""></option>
           <option value="sum">sum</option>
