13158f40b2cafee17e530245c130351addef75f0
rae charles <r_charles87@yahoo.com>
2021-11-12 02:20:18 -0500

    passed regular tests

--- src/App.js
+++ src/App.js
@@ -3,13 +3,114 @@ import Form from "./Form";
 import "./App.css";
 
 class App extends React.Component {
-  render() {
-    return (
+
+constructor(){
+  super()
+  this.state ={
+    userInput: "",
+    result: 0,
+    userSelectOption: '',
+  }
+}
+
+handleCalculateSubmit=(event)=>{
+  event.preventDefault()
+ if(this.state.userSelectOption === "sum"){
+   this.setState({
+     result: this.handleSum(this.state.userInput),
+     userInput: this.state.userInput
+   })
+   
+  
+ }
+ else if(this.state.userSelectOption === "average"){
+   this.setState({
+    result: this.handleAverage(this.state.userInput),
+    userInput: ""
+   })
+ }
+ else if(this.state.userSelectOption === "mode"){
+   this.setState({
+     result: this.handleMode(this.state.userInput)
+   })
+ }
+ else{
+   this.setState({
+     result: "Invalid input."
+   })
+ }
+
+
+}
+
+handleSelectChange=(event)=>{
+  this.setState({
+    userSelectOption: event.target.value
+  })
+}
+
+handleUserInput = (e) => {
+  let split = e.target.value.split(',').map((num)=>{
+    return Number(num)
+  })
+  this.setState({
+    userInput: split
+  })
+}
+
+handleSum=(numArr)=>{ 
+  let total = numArr.reduce((previousValue,currentValue)=> previousValue+currentValue)
+  // this.setState({
+  //   result: total
+  // })
+  return total
+}
+
+handleAverage=(numArr)=>{
+  return this.handleSum(numArr)/numArr.length
+}
+
+handleMode=(arr)=>{
+  const mode = {};
+  let max = 0, count = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    
+    if(mode[item]) {
+      mode[item]++;
+    } else {
+      mode[item] = 1;
+    }
+    
+    if(count < mode[item]) {
+      max = item;
+      count = mode[item];
+    }
+  }
+   
+  return max;
+}
+
+// put math here 
+
+render() {
+
+  // let total = this.state.userInput.reduce((previousValue,currentValue)=> previousValue+currentValue)
+  // console.log(total)
+
+
+// console.log(this.handleSum)
+  return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form calculate={this.handleCalculateSubmit} 
+        handleUserInput={this.handleUserInput} 
+        userInput={this.state.userInput}
+        handleSelectChange={this.handleSelectChange}
+        />
         <section id="result">
-          <p></p>
+          <p>{this.state.result}</p>
         </section>
       </main>
     );
--- src/Form.js
+++ src/Form.js
@@ -2,11 +2,32 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
+ 
+
+ 
+
+
+  
+  
+  
+  
   render() {
+    let {  calculate, userInput , handleUserInput, userSelectOption, handleSelectChange}= this.props
+    // console.log(this.state.userSelectOption)
+    // console.log(result)
+    // console.log(handleUserInput)
+    // console.log(userInput)
+    // console.log(this.props.handleSum.reduce((previousValue,currentValue)=> previousValue+currentValue))
+    // let total = ()=>this.handleSum(total)
+    // let total = userInput.reduce((previousValue,currentValue)=> previousValue+currentValue)
+    // console.log(total)
+
+
+
     return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={calculate}>
+        <input id="values" name="values" type="text" value={userInput} onChange={handleUserInput}/>
+        <select id="operation" name="operation" value={userSelectOption} onChange={handleSelectChange}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
