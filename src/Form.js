2500d8ef693a92d93b5d2a8838f9715dd85d0f57
“Lili <lilyhuang114classb@gmail.com>
2021-11-12 12:30:03 -0500

    Fixed the input issue.

--- src/Form.js
+++ src/Form.js
@@ -2,11 +2,83 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
+  constructor(){
+    super()
+    this.state ={
+      input: "",
+      operation: "",
+    }
+  }
+
+  handleInputBox =(event)=> {
+    this.setState ({
+      input: event.target.value
+    })
+  }
+
+  handleOperationChange =(event)=>{
+    this.setState({
+      operation: event.target.value
+    })
+  }
+
+  calcSum =(arr)=>{
+    let sum = 0;
+    for(let num of arr){
+      sum += num;
+    }
+    return sum;
+  }
+
+  calcMode = (arr) => {
+    let countObj = {};
+    for(let num of arr) {
+      //countObj[num] = (countOjb[num] || 0) + 1;
+      if(countObj[num]) {
+        countObj[num] += 1;
+      } else {
+        countObj[num] = 1;
+      }
+    }
+
+    let keys = Object.keys(countObj);
+    let hightValue = 0;
+    let hightestKey;
+    for(let key of keys){
+      if(countObj[key] > hightValue) {
+        hightValue = countObj[key];
+        hightestKey = key;
+      }
+    }
+    return hightestKey;
+  }
+  
+  handleSubmit =(event)=>{
+    event.preventDefault();
+    let newArr = this.state.input.split(",");
+    let numArr = newArr.map(str => Number(str));
+    let result = 0;
+    switch (this.state.operation) {
+      case "sum":
+        result = this.calcSum(numArr); 
+        break;
+      case "average":
+        result = this.calcSum(numArr)/numArr.length;
+      break;
+      case "mode":
+        result = this.calcMode(numArr)
+      break;
+    
+      default:
+        break;
+    }
+  }
+
   render() {
     return (
       <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+        <input id="values" name="values" type="text" value={this.state.input} onInput={this.handleInputBox}/>
+        <select id="operation" name="operation" value={this.state.value} onChange={this.handleOperationChange}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
@@ -14,6 +86,7 @@ class Form extends React.Component {
         </select>
         <button type="submit">Calculate</button>
       </form>
+      
     );
   }
 }
