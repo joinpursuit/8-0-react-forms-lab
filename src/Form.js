29053f86194bb8f11f8c81ca628cdab4d5c03445
Deborah Campos <631care@gmail.com>
2021-11-11 20:16:34 -0500

    some methods added, not solved or functional

--- src/Form.js
+++ src/Form.js
@@ -2,11 +2,47 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
+  constructor(){
+    super()
+    this.state ={
+      userInputArray: [],
+      sum: 0,
+      average: 0,
+      mode: 0
+    }
+
+  }
+calculateAvg=(event)=>{
+
+}
+calculateMode=(event)=>{
+
+}
+
+handleInputArray=(event)=>{
+    event.preventDefault();
+    let {userInputArray} = this.state 
+
+    this.setState({
+      userInputArray: event.target.value
+    })
+    console.log(userInputArray)
+  }
+  calculateSum=(event)=>{
+    let {userInputArray} = this.state 
+    let reducer =(previousValue, currentValue)=> previousValue + currentValue;   
+    let add =  userInputArray.reduce(reducer) 
+    this.setState({
+      sum: add   
+    })
+}
+
+
   render() {
     return (
       <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+        <input id="values" name="values" type="text" value = {this.state.userInputArray} onChange={this.handleInputArray}/>
+        <select id="operation" name="operation" value={this.state.sum} onChange={this.calculateSum}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
---
+++ src/Result.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Form from "react"
+
+class Result extends React.Component{
+    constructor(){
+        super();
+    }
+        
+
+   render(){
+
+       return(
+           <div>
+
+           </div>
+       )
+   }
+}
+
+export default Result
