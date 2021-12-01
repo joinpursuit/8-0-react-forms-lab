31d0537c5aacd9c13576853d0c1bb1062b49e88f
Larisa <larisashapkinab@gmail.com>
2021-12-01 12:07:19 -0500

    8-0-react-forms-lab-done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
+// import React from "react";
+// import Form from "./Form";
+// import "./App.css";
+
+// class App extends React.Component {
+//   constructor(){
+//     super();
+//     this.state={
+//       displayResult:0
+//     }
+//   }
+//   handleChangeResult=(result)=>{
+//     this.setState({
+//       displayResult: result
+//     })
+//   }
+//   render() {
+//     return (
+//       <main>
+//         <p>Enter each number in the array, separated by a ','</p>
+//         <Form handleChangeResult={this.handleChangeResult}/>
+//         <section id="result">
+//           <p>{this.state.displayResult}</p>
+//         </section>
+//       </main>
+//     );
+//   }
+// }
+
+// export default App;
+
 import React from "react";
 import Form from "./Form";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      displayResult: 0
+    }
+  }
+  handleChangeResult=(result)=>{
+    this.setState({
+      displayResult: result
+    })
+  }
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form handleChangeResult={this.handleChangeResult} />
         <section id="result">
-          <p></p>
+          <p>{ this.state.displayResult }</p>
         </section>
       </main>
     );
--- src/Form.js
+++ src/Form.js
@@ -2,11 +2,122 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      input: "",
+      operation: ""
+    }
+  }
+
+  handleInputChange=(event)=>{
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  handleOperationChange=(event)=>{
+    this.setState({
+      operation: event.target.value
+    })
+  }
+
+  calcSum=(arr)=>{
+    let sum = 0;
+    for(let num of arr){
+      sum += num;
+    }
+    return sum;
+  }
+
+  calcMode=(arr)=>{
+    let countObj = {};
+    for(let num of arr){
+      if(countObj[num]){
+        countObj[num] = countObj[num] + 1;
+      } else {
+        countObj[num] = 1;
+      }
+    }
+
+    let keys = Object.keys(countObj);
+    let highestValue=0;
+    let highestKey;
+    for(let key of keys){
+      if(countObj[key] > highestValue){
+        highestValue = countObj[key];
+        highestKey = key;
+      }
+    }
+
+    return highestKey;
+  }
+
+  handleValidation=(numArr)=>{
+    let flag = true;
+    if(this.state.input === ""){
+      flag = false;
+    }
+    for(let num of numArr){
+      if(isNaN(num)){
+        flag=false;
+      }
+    }
+    return flag;
+  }
+
+  handleSubmit=(event)=>{
+    event.preventDefault();
+    let strArr = this.state.input.split(",");
+    let numArr = strArr.map((str)=>Number(str));
+
+    let isValid = this.handleValidation(numArr);
+
+    if(this.state.operation === ""){
+      this.props.handleChangeResult("Invalid operation.");
+      return;
+    }
+
+    if(!isValid){
+      this.props.handleChangeResult("Invalid input.");
+      return;
+    }
+
+    let result = 0;
+    switch (this.state.operation) {
+      case "sum":
+        result = this.calcSum(numArr);
+        break;
+      case "average":
+        result = this.calcSum(numArr)/numArr.length;
+        break;
+      case "mode":
+        result = this.calcMode(numArr);
+        console.log("Calculate Mode");
+        break;
+      default:
+        break;
+    }
+
+    this.props.handleChangeResult(result);
+  }
+
   render() {
     return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          id="values" 
+          name="values" 
+          type="text"
+          value={this.state.input}
+          onInput={this.handleInputChange}
+        />
+        <select 
+          id="operation" 
+          name="operation"
+          value={this.state.operation}
+          onChange={this.handleOperationChange}
+        >
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
