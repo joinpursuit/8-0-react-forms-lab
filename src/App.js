8010e01e055bef77d20ca265712bc21853b6cbac
shareekaepps <shareekaepps@icloud.com>
2022-01-02 03:00:54 -0500

    finally fixed mode

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import Form from "./Form";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    input: "",
+    error: "",
+  };
+} 
+
+  numInput = ( entry, error ) => {
+    this.setState({
+    input: entry,
+    error: error
+  });
+}
+
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form numInput= { this.numInput } />
         <section id="result">
-          <p></p>
+        <p>{ this.state.input }</p>
+        <p>{ this.state.error }</p>
         </section>
       </main>
     );
--- src/Form.js
+++ src/Form.js
@@ -2,20 +2,97 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: [],
+      formula: "",
+      result: ""
+    };
+  }
+
+  handleInputChange = (e) => {
+    let user = e.target.value
+    this.setState({
+      input: user.split(",").map((i) => Number(i))
+    });
+  }
+
+  handleFormChange = (e) => {
+    this.setState({
+      formula: e.target.value
+    });
+  }
+
+  handleError = (e) => {
+    e.preventDefault();
+    switch (this.state.formula) {
+    case "sum": 
+    this.setState({
+    result: this.state.input.reduce((a, b) => a + b),
+    });
+
+    break;
+    case "average": 
+    this.setState({
+    result: this.state.input.reduce((a, b) => a +b) / 
+    this.state.input.length,
+    });
+
+    break;
+    case "mode": 
+    let mode = [];
+    let nums = {};
+
+    this.state.input.forEach((num) => 
+    nums[num] ? (nums[num] +=1) : (nums[num] = 1) && "Invalid input."
+    );
+
+    let total = 0;
+    for(let num in nums) {
+    if(Number(nums[num]) > total) {
+    total = Number(nums[num])
+    mode = [num];
+    } else if (
+    Number(nums[num]) === total) {
+    mode.push(num);
+    }
+  }
+    this.setState({
+    result: mode.join()
+    });
+    break;
+    default:
+
+    this.setState({
+    result: "Invalid input."
+    })
+  };
+}
+
   render() {
     return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
-          <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
-        </select>
-        <button type="submit">Calculate</button>
-      </form>
-    );
-  }
+    <>
+    <form>
+    <input id="values" name="values" type="text" onChange={ this.handleInputChange } />
+    <select id="operation" name="operation" onChange={ this.handleFormChange } >
+    
+    <option value=""></option>
+    <option value="sum">sum</option>
+    <option value="average">average</option>
+    <option value="mode">mode</option>
+    
+    </select>
+    <button type="submit" onClick={ this.handleError }>Calculate</button>
+    </form>
+    <div>
+
+    <p>{ this.state.input }</p>
+    <p>{ this.state.result }</p>
+
+    </div>
+    </>
+  )};
 }
 
 export default Form;
