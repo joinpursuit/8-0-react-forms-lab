0cff8270398cc29d7dfcf1b0c958a54c92aaa117
Christine Taylor <ctay47@icloud.com>
2022-05-12 18:19:52 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
+import { useState } from 'react';
 import Form from "./Form";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <p>Enter each number in the array, separated by a ','</p>
-        <Form />
-        <section id="result">
-          <p></p>
-        </section>
-      </main>
-    );
-  }
-}
+const App = () => {
+  const [calculation, setCalculation] = useState(null);
+  return (
+    <main>
+      <p>Enter each number in the array, separated by a ','</p>
+      <Form calculation={calculation} setCalculation={setCalculation} />
+      <section id="result">
+        <p>{calculation}</p>
+      </section>
+    </main>
+  );
+};
+
 
 export default App;
