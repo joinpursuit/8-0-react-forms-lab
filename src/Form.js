320861e90aa4ebecde1c35c153b932e8b6c7e944
T.K <nique@niques-air.myfiosgateway.com>
2022-05-11 21:51:58 -0400

    try

--- src/Form.js
+++ src/Form.js
@@ -2,7 +2,32 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      userInput: [],
+      operation: " ",
+      result: " ",
+    }
+  }
+
+  userInputHandle = (e) => {
+    this.setState({
+      userInput:
+      e.target.value.split(",")
+    })
+  }
+
+  operationHandle = (e) => {
+    this.setState({
+      operation:
+      e.target.value,
+    })
+  }
+
+
   render() {
+    const { result, userInput, operation } = this.state;
     return (
       <form>
         <input id="values" name="values" type="text" />
