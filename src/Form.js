25f45d5cbb35bdfad4bbbc159389f57fc4fae903
Jonathan Scheiber <jon@scheiber.me>
2022-05-13 00:10:58 -0400

    create form submit handler

--- src/Form.js
+++ src/Form.js
@@ -23,6 +23,51 @@ class Form extends React.Component {
     });
   };
 
+  handleSubmitForm = (event) => {
+    const { input, operation } = this.state;
+    event.preventDefault();
+
+    if (operation === "") {
+      this.setState({
+        result: "Invalid input.",
+      });
+    } else if (operation === "sum") {
+      this.setState({
+        result: input.reduce((a, b) => Number(a) + Number(b)),
+        input: [],
+        operation: "",
+      });
+    } else if (operation === "average") {
+      this.setState({
+        result: input.reduce((a, b) => Number(a) + Number(b)) / input.length,
+        input: [],
+        operation: "",
+      });
+    } else if (operation === "mode") {
+      let modeObject = {};
+      let mode = 1;
+      for (let number of input) {
+        if (!modeObject[number]) {
+          modeObject[number] = 1;
+        } else {
+          modeObject[number] += 1;
+        }
+      }
+
+      for (let number in modeObject) {
+        if (modeObject[number] > mode) {
+          mode = number;
+        }
+      }
+
+      this.setState({
+        result: mode,
+        input: [],
+        operation: "",
+      });
+    }
+  };
+
   render() {
     return (
       <form>
