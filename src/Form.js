9aaac0366afc613d2db0054e96ddda22a109c18e
Lortiz528 <lortiztech@gmail.com>
2022-05-13 00:00:55 -0400

    passes operations tests

--- src/Form.js
+++ src/Form.js
@@ -5,95 +5,149 @@ class Form extends React.Component {
   constructor() {
     super();
     this.state = {
+      numbers: [],
       result: '',
       operation: '',
       input: '',
-      error:'',
-      numbers: [],
     };
   }
 
+  validInputChecker = () => {
+    const { numbers, operation } = this.state;
+    if (numbers.length === 0) {
+      return false;
+    } else if (
+      numbers.some((num) => {
+        return Number.isNaN(num);
+      })
+    ) {
+      return false;
+    } else if (
+      operation !== 'sum' &&
+      operation !== 'average' &&
+      operation !== 'mode'
+    ) {
+      return false;
+    }
+    return true;
+  };
+
   getSum = (arr) => {
     let sum = 0;
-    for(let num of arr){
+    for (let num of arr) {
       sum += num;
     }
-    return sum
-  }
-  
-  // make a count countObj to keep track of how many of each num exists, then compare the values
+    return sum;
+  };
+
+  // make a countObj to keep track of how many of each num exists, then make an array to find the key with hightest value
   getMode = (arr) => {
     let countObj = {};
-    for(let num of arr){
-      if(countObj[num]){
+    for (let num of arr) {
+      if (countObj[num]) {
         countObj[num]++;
-      }else{
+      } else {
         countObj[num] = 1;
       }
     }
-
-    let keys = Object.keys(countObj);
-    let highestValue = 0;
-    let highestKey;
-    for(let key of keys){
-      if(countObj[key] > highestValue){
-        highestValue = countObj[key];
-        highestKey = key;
+    // at this point, countObj is made up of the digits of user input as keys and the # of times they appear as their values.
+    let newArr = Object.entries(countObj);
+    let highest = newArr[0];
+    for (let i = 1; i < newArr.length; i++) {
+      if (newArr[i][1] > highest[1]) {
+        highest = newArr[i];
       }
     }
-    return highestKey
-  }
-  
-  handleFormSubmit = (event) => {
-    const { operation, input} = this.state;
-    event.preventDefault();
+    return highest[0];
 
-    let arr = this.state.input.split(',').map((num) => Number(num));
-
-    if (input === '' || input !== Number) {
-      this.setState({
-        error: "Invalid input."
-      })
+    // alternative
+    // let keys = Object.keys(countObj);
+    // let highestValue = 0;
+    // let highestKey;
+    // for(let key of keys){
+    //   if(countObj[key] > highestValue){
+    //     highestValue = countObj[key];
+    //     highestKey = key;
+    //   }
+    // }
+    // return highestKey
   };
-if (operation === "sum") {
-  this.setState({
-    result: this.getSum(arr)
 
-  })
-}
+  operationHandler = (event) => {
+    const { value } = event.target;
+    this.setState({
+      operation: value,
+    });
+  };
 
-if (operation === 'average') {
-  this.setState({
-    result: this.getSum(arr)/arr.length
-  })
-}
-}
+  inputHandler = (event) => {
+    const { value } = event.target;
+    // take apart the numbers inputted and put them back into an array in #form
+    const arr = value.split(',').map((num) => Number(num));
+    this.setState({
+      numbers: arr,
+      input: value,
+    });
+  };
 
-// inputhandler - whatever user types in becomes the new value of this.state.input
-inputHandler = (event) => {
-  this.setState({
-    input: event.target.value
-  })
-}
+  formSubmitHandler = (event) => {
+    event.preventDefault();
+    const { operation, numbers } = this.state;
 
-operationHandler = (event) => {
-  this.setState({
-    operation: event.target.value
-  })
-}
+    if (!this.validInputChecker) {
+      this.setState({
+        result: 'Invalid input.',
+      });
+    } else {
+      if (operation === 'sum') {
+        this.setState({
+          result: this.getSum(numbers),
+          input: '',
+          operation: '',
+        });
+      } else if (operation === 'average') {
+        this.setState({
+          result: this.getSum(numbers) / numbers.length,
+          input: '',
+          operation: '',
+        });
+      } else {
+        this.setState({
+          result: this.getMode(numbers),
+          input: '',
+          operation: '',
+        });
+      }
+    }
+  };
 
   render() {
+    const { result, input, operation } = this.state;
     return (
-      <form onSubmit={this.handleFormSubmit}>
-        <input onChange={this.inputHandler} id="values" name="values" type="text" />
-        <select onChange={this.operationHandler} id="operation" name="operation">
-          <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
-        </select>
-        <button type="submit">Calculate</button>
-      </form>
+      <>
+        <form onSubmit={this.formSubmitHandler}>
+          <input
+            onChange={this.inputHandler}
+            id="values"
+            name="values"
+            type="text"
+            value={input}
+          />
+          <select
+            onChange={this.operationHandler}
+            id="operation"
+            name="operation"
+            value={operation}
+          >
+            <option value=""></option>
+            <option value="sum">sum</option>
+            <option value="average">average</option>
+            <option value="mode">mode</option>
+          </select>
+          <button type="submit">Calculate</button>
+        </form>
+        <p>{result}</p>
+      </>
     );
   }
 }
