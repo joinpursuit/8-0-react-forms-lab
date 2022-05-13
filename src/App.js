5bb637588ec112ba5eb653355aee432668df1363
Jimmy Ayivor <jimmyayivor@gmail.com>
2022-05-13 08:05:40 -0400

    Solution to 8-0-react-forms-lab

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <p>Enter each number in the array, separated by a ','</p>
-        <Form />
-        <section id="result">
-          <p></p>
-        </section>
-      </main>
-    );
-  }
+function App() {
+
+  const [result, setResult] = useState("");
+
+  return (
+    <main>
+      <p>Enter each number in the array, separated by a ','</p>
+      <Form
+        handleResult={result => setResult(result)}
+      />
+      <section id="result">
+        <p>{result}</p>
+      </section>
+    </main>
+  );
 }
+
 
 export default App;
