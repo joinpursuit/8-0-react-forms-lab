6915e2b99d968c4d41ae76a13fa252bf33fd499c
sabrina <sabrinaescobarflores@gmail.com>
2022-05-12 21:58:49 -0400

    Passing the form test

--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,76 @@
-import React from "react";
+import{ useState } from "react";
 import "./Form.css";
 
-class Form extends React.Component {
-  render() {
+const Form = () => {
+    const [numbers, setNumbers] = useState(0);
+    const [select, setSelect] = useState("");
+    const [result, setResult] = useState();
+    const getNumbers = (e) => {
+    let numArr = [];
+     
+    const str = e.target.value;
+      numArr = str.split(',');
+      setNumbers(numArr)
+    }
+
+    const handleSelect = (e) => {
+       const {value} = e.target;
+       setSelect(value);
+    }
+
+    const handleSubmit = (e) => {
+        let sum = 0;
+        if (select === "") {
+          setResult('Invalid input.')
+        } else if (select === 'sum') {
+        numbers.forEach(num => {
+          sum += Number(num);
+        });
+        if (!isNaN(sum)) {
+        setResult(sum);
+        } else {
+          setResult('Invalid input.')
+        }
+      } else if (select === 'average') {
+        numbers.map(num => {
+        return sum += Number(num);
+        });
+        setResult(sum/numbers.length)
+      } else if (select === 'mode') {
+        let counter = 0;
+        let count = 0;
+        numbers.filter(num => {
+        count = 0
+        numbers.forEach(mode => {
+            if (num === mode) {
+               count++ 
+            }
+            return count;
+        })
+        if (count > counter) {
+            counter = count;
+            setResult(num);
+        }
+        return result;
+    });
+      }
+      e.preventDefault();
+      } 
     return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <>
+      <form onSubmit={handleSubmit}>
+        <input id="values" name="values" type="text"  value={numbers} onChange={getNumbers}/>
+        <select id="operation" name="operation" onChange={handleSelect} value = { select }>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
           <option value="mode">mode</option>
         </select>
-        <button type="submit">Calculate</button>
+        <button type="submit" >Calculate</button>
       </form>
-    );
+       <p> {result}</p> 
+       </>
+    );  
   }
-}
 
 export default Form;
