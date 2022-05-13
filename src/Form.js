17cd922eb047384f7017b5069ceae14dc73003c1
Samantha Figueroa <samanthafigueroa@pursuit.org>
2022-05-12 23:07:10 -0400

    lab done

--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
-class Form extends React.Component {
-  render() {
-    return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
-          <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
-        </select>
-        <button type="submit">Calculate</button>
-      </form>
-    );
-  }
-}
+const Form = (props) => {
+  const { calculation, setCalculation } = props;
+
+  const [numbers, setNumbers] = useState("");
+
+  const handleNumbers = (e) => {
+    const { value } = e.target;
+    setNumbers(value);
+  };
+
+  const [select, setSelect] = useState("");
+
+  const handleSelect = (e) => {
+    const { value } = e.target;
+    setSelect(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //include comma to split at every comma
+    if(numbers === ''){
+      setCalculation('Invalid input.')
+    }
+    const numArr = numbers.split(",");
+
+    numArr.forEach(num => {
+      if(!Number(num)){
+        setCalculation('Invalid input.')
+      }
+    })
+
+    if (select === "sum") {
+      setCalculation(
+        numArr.reduce((a, b) => Number(a) + Number(b))
+      );
+    } else if (select === "average") {
+      setCalculation(
+        numArr.reduce((a, b) => Number(a) + Number(b)) / numArr.length)
+    } else if (select === "mode") {
+      const obj = {};
+      let highestValue = 0;
+      let highestValueKey = -Infinity;
+
+      numArr.forEach( number => {
+        if(!obj[number]){
+          obj[number] = 1
+        } else{
+          obj[number] += 1
+        }
+      })
+
+      for(let key in obj){
+        const value = obj[key];
+        if(value > highestValue){
+          highestValue = value;
+          highestValueKey = key;
+        }
+      }
+      setCalculation(Number(highestValueKey))
+    }
+
+    setNumbers('')
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        id="values"
+        name="values"
+        type="text"
+        value={numbers}
+        onChange={handleNumbers}
+      />
+      <select
+        id="operation"
+        name="operation"
+        value={select}
+        onChange={handleSelect}
+      >
+        <option value=""></option>
+        <option value="sum">sum</option>
+        <option value="average">average</option>
+        <option value="mode">mode</option>
+      </select>
+      <button type="submit">Calculate</button>
+    </form>
+  );
+};
 
 export default Form;
