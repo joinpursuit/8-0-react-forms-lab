9c4fb4df0efb5fb9bc198702519dcd8679ab2e76
masmei <masonmei@pursuit.org>
2022-05-14 00:19:58 -0500

    finished :)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
+0
+import React, { useState } from "react";
 import Form from "./Form";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <p>Enter each number in the array, separated by a ','</p>
-        <Form />
-        <section id="result">
-          <p></p>
-        </section>
-      </main>
-    );
-  }
-}
+const App = () => {
+  const [answer, setAnswer] = useState("");
+  return (
+    <main>
+      <p>Enter each number in the array, separated by a ','</p>
+      <Form setAnswer={setAnswer}/>
+      <section id="result">
+        <p>{answer}</p>
+      </section>
+    </main>
+  );
+};
 
 export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
-class Form extends React.Component {
-  render() {
-    return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
-          <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
-        </select>
-        <button type="submit">Calculate</button>
-      </form>
-    );
-  }
-}
+const Form = (props) => {
+  const [input, setInput] = useState("");
+  const [select, setSelect] = useState("");
+  const { setAnswer } = props;
+  // ____________________________________________
+
+  const handleInput = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+
+  const handleSelect = (e) => {
+    // const value = e.target.value
+    const { value } = e.target;
+    setSelect(value);
+  };
+
+  let answer = 0;
+
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!input) {
+      setAnswer("Invalid input.")
+    }
+    if (!select) {
+      setAnswer("Invalid input.")
+    } else {
+      let result = math(input, select)
+      setAnswer(result)
+    }
+  
+  };
+  
+  const math = (input, select) => {
+    if (select === "sum") {
+      let sum = 0;
+      let nums = input.split(",");
+      nums.map((num) => {
+        sum += (num*1)
+      });
+      return sum
+    }
+    if (select === "average") {
+      let sum = 0;
+      let nums = input.split(",");
+      nums.map((num) => {
+        sum += (num*1)
+      });
+      return (sum / nums.length)
+    }
+    if(select === "mode") {
+      let nums = input.split(",");
+      
+      function getMode(array){
+        const obj = {};
+        array.forEach(number => {
+          if(!obj[number]) {
+            obj[number] = 1;
+          } else {
+            obj[number] += 1;
+          }
+        })
+        let highestValue = 0;
+        let highestValueKey = -Infinity;
+
+        for(let key in obj){
+          const value = obj[key];
+          if(value > highestValue) {
+            highestValue = value;
+            highestValueKey = key;
+          }
+        }
+        return Number(highestValueKey)
+      }
+      return getMode(nums) 
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        id="values"
+        name="values"
+        type="text"
+        value={input}
+        onChange={handleInput}
+      />
+      <select
+        id="operation"
+        name="operation"
+        onChange={handleSelect}
+        value={select}
+      >
+        <option value=""></option>
+        <option value="sum">sum</option>
+        <option value="average">average</option>
+        <option value="mode">mode</option>
+      </select>
+      <button type="submit">Calculate</button>
+    </form>
+  );
+};
 
 export default Form;
