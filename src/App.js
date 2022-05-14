3c497d475c65455d6bb1db3cb47ad078e7149a1b
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2022-05-14 11:48:48 -0400

    react forms finished

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import Form from "./Form";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
+const App = () => {
+  const [answer, setAnswer] = useState("");
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form setAnswer={ setAnswer }/>
         <section id="result">
-          <p></p>
+          <p>{ answer }</p>
         </section>
       </main>
     );
   }
-}
+
 
 export default App;
