0909af55907db87cd2a9542ce61fc83ab63a2961
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-05-13 21:47:08 -0400

    "finished"

--- src/Form.js
+++ src/Form.js
@@ -1,82 +1,117 @@
-import { useState } from "react";
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
-  
-  const Form = () => {
-    const [input, setInput] = useState("");
-    const [select, setSelect] = useState("");
-    const [result, setResult] = useState("");
-  
-    const handleInput = (e) => {
-      const { value } = e.target;
-      setInput(value);
-    };
-    const handleSelect = (e) => {
-      const { value } = e.target;
-      setSelect(value);
-    };
-    const handleResult = (e) => {
-      const { value } = e.target
-      if(!input) {
-        setResult(value = 'Invalid input.')
-      }
-    };
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const { value } = e.target;
-    };
-  
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            id="values"
-            name="values"
-            type="text"
-            onChange={handleInput}
-            value={input}
-          />
-          <select
-            id="operation"
-            name="operation"
-            onChange={handleSelect}
-            value={select}
-          >
-            <option value=""></option>
-            <option value="sum">sum</option>
-            <option value="average">average</option>
-            <option value="mode">mode</option>
-          </select>
-          <button type="submit">Calculate</button>
-        </form>
-        <h3 onChange={handleResult}>{result}</h3>
-        <p>{!input ? "" : "Invalid input."}</p>
-        <p>{!input ? "Invalid input." : ""}</p>
-      </div>
-    );
+
+const Form = (props) => {
+  const [textInput, setTextInput] = useState("");
+  const [selectedOperation, setSelectedOperation] = useState("");
+  const [result, setResult] = useState("");
+  const [className, setErrorClassName] = useState("");
+
+  const handleTextInput = (e) => {
+    const { value } = e.target;
+    setTextInput(value);
   };
-  
-  export default Form;
+
+  const handleSelectedOperation = (e) => {
+    const { value } = e.target;
+    setSelectedOperation(value);
+  };
+
+  // ADD THE VALUES TO THE RESUKT
+  const handleResult = () => {
+    props.handleResult(result);
+  };
 
 
+  // SET ALL FIELDS TO NOTHING
+  const clearFields = () => {
+    setTextInput('');
+    setSelectedOperation('');
+    setErrorClassName('');
+  };
+
+  // the box with all an array of values - textInput.split(',').map(i => i * 1); - TURNS 1,2,3 INTO [1,2,3]
+  const handleSubmit = (e) => {
+    const myArray = textInput.split(',').map(i => i * 1);
+    // SUM OF THE ARRAY - WITH OPERATION DROP DOWN 
+    const sum = myArray.reduce((a, b) => (a + b));
+    // AVERAGE OF ARRA
+    const average = Math.floor(sum / myArray.length);
+
+    const mode = getMode(myArray);
 
 
-// class Form extends React.Component {
-//   render() {
-//     return (
-//       <form>
-//         <input id="values" name="values" type="text" />
-//         <select id="operation" name="operation">
-//           <option value=""></option>
-//           <option value="sum">sum</option>
-//           <option value="average">average</option>
-//           <option value="mode">mode</option>
-//         </select>
-//         <button type="submit">Calculate</button>
-//       </form>
-//     );
-//   }
-// }
+    // PREVENT PAGE FROM REFRESHING WITH OPERATION DROPDOWN IS CHOSEN
+    e.preventDefault();
 
-// export default Form;
+    // ERROR MESSAGE FOR NO TEXT OR OPERATION NUMBER CHOSEN - RETURN INPUT IS INVALID - LEAVE THE WRONG TEXT - RUN ERROR MESSAGE
+    if (!textInput || !selectedOperation || Number.isNaN(myArray[0])) {
+      setResult(`Invalid input.`);
+      setTextInput(textInput);
+      setErrorClassName('error');
+
+      // IF THERE IS TEXT AND OPERATION CHOSEN IS SUM - THEN CLEAR THE FIELDS
+    } else if (textInput && selectedOperation === "sum") {
+      setResult(sum);
+      clearFields();
+
+      // IF THERE IS TEXT AND OPERATION CHOSEN IS AVERAGE - THEN CLEAR THE FIELDS
+
+    } else if (textInput && selectedOperation === "average") {
+      setResult(average);
+      clearFields();
+
+    } else {
+      setResult(mode);
+      clearFields();
+    }
+  };
+
+  // TAKE THE ARRAY AND CREATE AN OBJECT WITH THE VALUES
+  const getMode = (myArray) => {
+    let myModeObj = {};
+    let curentKey;
+    let highestCount = 0;
+
+    // IF THE ITERATION THROUGH THE ARRAY HAS ONE OCCURANCE GIVE IT A KEY OF 1
+    myArray.forEach(el => {
+      if (!myModeObj[el]) {
+        myModeObj[el] = 1;
+      } else {
+        // ELSE IF THE OCCURANCE IS MORE THAT 1 ADD 2 AS ITS KEY
+        myModeObj[el] += 1;
+      }
+    });
+
+    // ITERATE THROUGH THE OBJECT KEY VALUES AND RETURN THE KEY WITH TH MOST ITERATION VALUES
+    Object.entries(myModeObj).forEach(([k, v]) => {
+      if (v > highestCount) {
+        curentKey = k;
+        highestCount = v;
+      }
+    });
+    return curentKey;
+  };
+
+
+  return (
+    <form onSubmit={handleSubmit} >
+      <input id="values" className={className} name="values" type="text" onChange={handleTextInput} value={textInput} />
+      <select id="operation" name="operation"
+        onChange={handleSelectedOperation}
+        value={selectedOperation}
+        className={className}
+      >
+        <option value=""></option>
+        <option value="sum">sum</option>
+        <option value="average">average</option>
+        <option value="mode">mode</option>
+      </select>
+      <button type="submit" onClick={handleResult()}>Calculate
+      </button>
+    </form>
+  );
+}
+export default Form;
+
