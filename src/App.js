2ac14490fab1de1fa339bedf9916a59212f9e2a1
T.K <nique@niques-air.myfiosgateway.com>
2022-05-15 13:46:35 -0400

    retry

--- src/App.js
+++ src/App.js
@@ -2,18 +2,40 @@ import React from "react";
 import Form from "./Form";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <p>Enter each number in the array, separated by a ','</p>
-        <Form />
-        <section id="result">
-          <p></p>
-        </section>
-      </main>
-    );
-  }
+function App() {
+  const [result, setResult] = useState("");
+
+  return (
+    <main>
+      <p>Enter each number in the array, separated by a ','</p>
+      <Form 
+        setResult = { setResult }
+      />
+      <section id="result">
+        <p>{ result }</p>
+      </section>
+    </main>
+  );
 }
+
+
+
+
+
+
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <main>
+//         <p>Enter each number in the array, separated by a ','</p>
+//         <Form />
+//         <section id="result">
+//           <p></p>
+//         </section>
+//       </main>
+//     );
+//   }
+// }
 
 export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,46 +1,91 @@
 import React from "react";
+import { useState } from "react/cjs/react.production.min";
 import "./Form.css";
 
-class Form extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      userInput: [],
-      operation: " ",
-      result: " ",
-    }
+const Form = ({ setResult }) => {
+  const [userInput, setUserInput] = useState("");
+  const [selectOp, setSelectOp] = useState("");
+
+  const handleUserInput = (e) => {
+    const { value } = e.target;
+    setUserInput(value);
+  };
+
+  const handleSelectOp = (e) => {
+    const { value } = e.target;
+    setSelectOp(value);
+  };
+
+  const handleSubmit = (e) => {
+    const arr = userInput.split(",").map(i => Number(i));
+    const sum = arr.reduce((a, b) => (a + b));
+    const average = Math.floor(sum / arr.length);
   }
 
-  userInputHandle = (e) => {
-    this.setState({
-      userInput:
-      e.target.value.split(",")
-    })
-  }
-
-  operationHandle = (e) => {
-    this.setState({
-      operation:
-      e.target.value,
-    })
-  }
+  e.preventDefault();
 
 
-  render() {
-    const { result, userInput, operation } = this.state;
-    return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
-          <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
-        </select>
-        <button type="submit">Calculate</button>
-      </form>
-    );
-  }
+
+
+  return (
+    <form onSubmit={ handleSubmit }>
+      <input id="values" name="values" type="text" onChange={ handleUserInput } value={ userInput }/>
+      <select id="operation" name="operation" onChange={ handleSelectOp } value={ selectOp }>
+        <option value=""></option>
+        <option value="sum">sum</option>
+        <option value="average">average</option>
+        <option value="mode">mode</option>
+      </select>
+      <button type="submit">Calculate</button>
+    </form>
+  );
 }
+
+
+
+
+
+// class Form extends React.Component {
+//   constructor(){
+//     super()
+//     this.state = {
+//       userInput: [],
+//       operation: " ",
+//       result: " ",
+//     }
+//   }
+
+//   userInputHandle = (e) => {
+//     this.setState({
+//       userInput:
+//       e.target.value.split(",")
+//       })
+//     }
+//   }
+
+//   operationHandle = (e) => {
+//     this.setState({
+//       operation:
+//       e.target.value,
+//     })
+//   }
+
+
+  // render() {
+  //   const { result, userInput, operation } = this.state;
+  //   return (
+  //     <form>
+  //       <input id="values" name="values" type="text" />
+  //       <select id="operation" name="operation">
+  //         <option value=""></option>
+  //         <option value="sum">sum</option>
+  //         <option value="average">average</option>
+  //         <option value="mode">mode</option>
+  //       </select>
+  //       <button type="submit">Calculate</button>
+  //     </form>
+  //   );
+  // }
+// }
 
 export default Form;
