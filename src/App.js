68fa1c99c5677a237428223414b539a0b7bef7a9
Khaleed Uddin <uddin1786@gmail.com>
2022-05-17 22:00:21 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
+import {useState} from "react";
 import Form from "./Form";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
+const App = () => {
+  
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
         <Form />
         <section id="result">
-          <p></p>
+          <p>{}</p>
         </section>
       </main>
     );
-  }
+  
 }
 
 export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,44 @@
-import React from "react";
+import {useState} from "react";
 import "./Form.css";
 
-class Form extends React.Component {
-  render() {
+const Form = () => {
+const [numArray, setnumArray] = useState('')
+
+const handleArray = (e) =>{
+  const {value} = e.target
+  // alert( value)
+setnumArray(value.split(",").map(i => +i))
+}
+
+const handleOperation = (e) => {
+  const {value} = e.target;
+  if(value === "sum"){
+    setnumArray( numArray.reduce((a, b) => a + b) )
+  }
+  if(value === "average")  {
+    setnumArray(numArray.reduce((a, b) => a + b)/ numArray.length )
+  }  
+
+
+}
     return (
       <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+        <h1>{numArray}</h1>
+        <input id="values" name="values" type="text" 
+        onChange={handleArray}
+        />
+        <select id="operation" name="operation"
+        onChange={ handleOperation }
+        >
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
           <option value="mode">mode</option>
         </select>
-        <button type="submit">Calculate</button>
+        <button type="submit"> This Calculate button is useless lol</button>
       </form>
     );
-  }
+  
 }
 
 export default Form;
