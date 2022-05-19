285427897091bc2bb822eba588d4fc5af69a491c
bowersjuan <bowersjuan@gmail.com>
2022-05-19 01:58:14 -0400

    Update App.js and Form.js with validator and operation functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,84 @@ import Form from "./Form";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      operation: null,
+      numbersArray: [],
+      result: null,
+    };
+  }
+
+  handleNumbersChange = (e) => {
+    const { value } = e.target;
+    this.setState({
+      numbersArray: value.split(",").map((num) => {
+        return parseInt(num);
+      }),
+    });
+  };
+
+  handleOperationChange = (e) => {
+    const { value } = e.target;
+    this.setState({
+      operation: value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    const { operation, numbersArray } = this.state;
+
+    e.preventDefault();
+    if (operation && numbersArray.length > 0) {
+      this.setState({
+        result: "Calculation result:",
+      });
+
+      if (operation === "sum") {
+        let sumRes = numbersArray.reduce((acc, num) => acc + num, 0);
+
+        this.setState({
+          result: sumRes,
+        });
+      } else if (operation === "average") {
+        let avgRes =
+          numbersArray.reduce((acc, num) => acc + num, 0) / numbersArray.length;
+
+        this.setState({
+          result: avgRes,
+        });
+      } else if (operation === "mode") {
+        const hashMap = {};
+        numbersArray.forEach((num) => {
+          if (!hashMap[num]) {
+            hashMap[num] = 0;
+          }
+          hashMap[num] += 1;
+        });
+
+        let modeRes = Object.keys(hashMap).reduce((a, b) =>
+          hashMap[a] >= hashMap[b] ? a : b
+        );
+
+        this.setState({
+          result: modeRes,
+        });
+      }
+    }
+  };
+
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form
+          handleSubmit={this.handleSubmit}
+          handleOperation={this.handleOperationChange}
+          handleNumbers={this.handleNumbersChange}
+        />
         <section id="result">
-          <p></p>
+          <p>{this.state.result ? this.state.result : "Invalid input."}</p>
         </section>
       </main>
     );
--- src/Form.js
+++ src/Form.js
@@ -4,9 +4,18 @@ import "./Form.css";
 class Form extends React.Component {
   render() {
     return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={this.props.handleSubmit}>
+        <input
+          onChange={this.props.handleNumbers}
+          id="values"
+          name="values"
+          type="text"
+        />
+        <select
+          onChange={this.props.handleOperation}
+          id="operation"
+          name="operation"
+        >
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
