f4a7d67849ac60ba66abd96d45afa9c082c4dd97
John Prado <jcprado20@icloud.com>
2022-05-25 16:38:47 -0400

    practice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
+// import PracticeForm from "./PracticeForm";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <p>Enter each number in the array, separated by a ','</p>
-        <Form />
-        <section id="result">
-          <p></p>
-        </section>
-      </main>
-    );
-  }
+function App() {
+
+  const [result, setResult] = useState("");
+
+  return (
+    <main>
+      <p>Enter each number in the array, separated by a ','</p>
+      <Form setResult = {setResult}/>
+      {/* <PracticeForm setResult={setResult}/> */}
+      <section id="result">
+        <p>{result}</p>
+      </section>
+    </main>
+  );
 }
 
-export default App;
+
+export default App
+
+//Statistics is the measure of Gods will
--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
-class Form extends React.Component {
-  render() {
-    return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
-          <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
-        </select>
-        <button type="submit">Calculate</button>
-      </form>
-    );
-  }
-}
 
+
+const Form = ({ setResult }) => {
+  const [textInput, setTextInput] = useState("");
+  const [selectedOperation, setSelectedOperation] = useState("");
+  const [className, setErrorClassName] = useState("");
+
+  const handleTextInput = (e) => {
+    const { value } = e.target;
+    setTextInput(value);
+  };
+
+  const handleSelectedOperation = (e) => {
+    const { value } = e.target;
+    setSelectedOperation(value);
+  };
+
+  const clearFields = () => {
+    setTextInput('');
+    setSelectedOperation('');
+    setErrorClassName('');
+  };
+
+  const handleSubmit = (e) => {
+    const myArray = textInput.split(',').map(i => i * 1); //Number or toInt .map(i => Number(i))
+    const sum = myArray.reduce((a, b) => (a + b)); // 12345
+    const average = Math.floor(sum / myArray.length);
+    const mode = getMode(myArray);
+
+    e.preventDefault();
+
+    if (!textInput || !selectedOperation || Number.isNaN(myArray[0])) {
+      setResult('Invalid input.');
+      setTextInput(textInput);
+      setErrorClassName('error');
+
+    } else if (textInput && selectedOperation === "sum") {
+      setResult(sum);
+      clearFields();
+
+    } else if (textInput && selectedOperation === "average") {
+      setResult(average);
+      clearFields();
+
+    } else {
+      setResult(mode);
+      clearFields();
+    }
+  };
+
+  const getMode = (array) => {
+    const frequency = {}
+    array.forEach((el) => {
+      if (frequency[el]) {
+        frequency[el] += 1
+      } else {
+        frequency[el] = 1
+      }
+    })
+    const modeValue = Object.entries(frequency).sort((a, b) => {
+      return b[1] - a[1]
+    }) 
+    return modeValue[0][0]
+  }
+  
+  return (
+    <form onSubmit={handleSubmit} >
+      <input id="values" className={className} name="values" type="text" onChange={handleTextInput} value={textInput} />
+      <select id="operation" name="operation"
+        onChange={handleSelectedOperation}
+        value={selectedOperation}
+        className={className}
+        >
+        <option value=""></option>
+        <option value="sum">sum</option>
+        <option value="average">average</option>
+        <option value="mode">mode</option>
+      </select>
+      <button type="submit" >Calculate
+      </button>
+    </form>
+  );
+}
 export default Form;
+
+
+
+
+
+
+
+
+// const getMode = (myArray) => {
+//   let myModeObj = {};
+//   let curentKey;
+//   let highestCount = 0;
+
+//   myArray.forEach(el => {
+//     if (!myModeObj[el]) {
+//       myModeObj[el] = 1;
+//     } else {
+//       myModeObj[el] += 1;
+//     }
+//   });
+
+//   Object.entries(myModeObj).forEach(([k, v]) => {
+//     if (v > highestCount) {
+//       curentKey = k;
+//       highestCount = v;
+//     }
+//   });
+//   return curentKey;
+// };
+// var numbers = [1,2,2,3,4,5];
+// var counts = numbers.reduce((counts, e) => { counts[e] = counts[e] ? counts[e] + 1 : 1; return counts; }, {});
+// var mode = Object.keys(counts).reduce((a, b) => (counts[a] > counts[b] ? a : b ));
+// console.log(mode);
