1783e80004b9a50b1ca3e66ccdfddaba77fa14e2
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2022-06-28 22:16:32 -0400

    submit lab

--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,28 @@
 import React from "react";
 import "./Form.css";
-
-class Form extends React.Component {
-  render() {
-    return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
-          <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
-        </select>
-        <button type="submit">Calculate</button>
-      </form>
-    );
+// [1,3,3,7]
+function  Form() {
+  const add = (nums) => {
+    let total = 0;
+   nums.foreach(total)
   }
+  return (
+    <form>
+      <input id="values" name="values" type="text" />
+      <select id="operation" name="operation">
+        <option value=""></option>
+        <option value="sum">sum</option>
+        <option value="average">average</option>
+        <option value="mode">mode</option>
+      </select>
+      <button type="submit">Calculate</button>
+    </form>
+  );
 }
+
+
+ 
+   
+
 
 export default Form;
