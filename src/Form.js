b27837fe02961c5556e736cbd1e0247a3b9afed0
Bo Latt <bolattny@gmail.com>
2022-10-09 16:07:32 -0400

    passed the tests

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
-function Form() {
+function Form({ setResult }) {
+  const operations = {
+    sum: function (values) {
+      return values.split(",").reduce((a, b) => a + Number(b), 0);
+    },
+    average: function (values) {
+      return this.sum(values) / values.split(",").length;
+    },
+    mode: function (values) {
+      values = values.trim().replaceAll(",", "").replaceAll(" ", "");
+      let mode = 0;
+      let occurence = 1;
+      values.split("").forEach((value) => {
+        if (values.split(value).length > occurence) {
+          mode = value;
+          occurence = values.split(value).length;
+        }
+      });
+      return mode;
+    },
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.values.value);
+    console.log(e.target.operation.value);
+
+    let values = e.target.values.value;
+    const operation = e.target.operation.value;
+    if (!values || !operation) {
+      return setResult("Invalid input.");
+    }
+    console.log(operations[operation](values));
+
+    const result = operations[operation](values);
+    setResult(result);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input id="values" name="values" type="text" />
       <select id="operation" name="operation">
         <option value=""></option>
