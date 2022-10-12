cf9c6374286b702fbb2aa2cf78a40755ac5eba57
Salinamalek <salinamalek@pursuit.org>
2022-10-12 17:37:09 -0400

    minor progress/start of lab

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import "./Form.css";
 
 function Form() {
+  const [selectOpt, setSelectOpt] = useState("");
+
+  const [input, setInput] = useState("");
+
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+    <form type="submit">
+      <input
+        id="values"
+        name="values"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <select
+        id="operation"
+        name="operation"
+        value={selectOpt}
+        onChange={(e) => setSelectOpt(e.target.value)}
+      >
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
