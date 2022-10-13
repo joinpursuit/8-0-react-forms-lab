826407dd904c8f81802242cb49a96123692bf42b
TonyH98 <tonyhoang@pursuit.org>
2022-10-12 21:03:51 -0400

    Some tests passed

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,56 @@
 import React from "react";
 import "./Form.css";
+import {useState} from "react"
+
 
 function Form() {
+
+const [array , setArray] = useState([])
+const [selectOption , setSelectOption] = useState("")
+
+const handleChange = (event) => {
+setArray(event.target.value.split(","))
+}
+
+function calculateValue(){ 
+  let sum = 0 
+    if(selectOption === "sum"){
+      array.forEach((num) => {
+        sum += Number(num)
+     })
+    }
+   if(selectOption === "average"){
+    array.forEach((num) => {
+     sum += Number(num)
+     return ((sum / array.length))
+    })
+  }
+     
+   document.querySelector("#number").innerHTML = sum === "number" ? sum : "Invalid input."
+}
+
+  function reset(){
+    setArray("")
+    setSelectOption("")
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    calculateValue()
+    reset()
+  }
+
+
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+    <form onSubmit={handleSubmit}>
+      <input 
+      id="values" 
+      name="values" 
+      type="text" 
+      value={array}
+      onChange={handleChange}
+      />
+      <select id="operation" name="operation" value={selectOption} onChange={(e) => setSelectOption(e.target.value)}>
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
