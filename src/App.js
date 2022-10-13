e316e5d872c0fb919e6c1a0b3a10df9c50e83204
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-10-12 23:55:28 -0400

    good progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 import "./App.css";
 
 function App() {
+  const [appResult, setAppResult] = useState("");
+
   return (
     <main>
       <p>Enter each number in the array, separated by a ','</p>
-      <Form />
+      <Form changeResult={(appResult) => setAppResult(appResult)} />
       <section id="result">
-        <p></p>
+        <p>{appResult}</p>
       </section>
     </main>
   );
--- src/Form.js
+++ src/Form.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import "./Form.css";
 
-function Form() {
+function Form(props) {
   const [numbers, setNumbers] = useState([]);
   const [operator, setOperator] = useState("");
 
@@ -13,16 +13,37 @@ function Form() {
   const calculate = (event) => {
     event.preventDefault();
     let result;
+    let modeNum;
+    let modeOccurrence = 0;
     if (operator === "sum" || operator === "average") {
       result = numbers.reduce((acc, el) => {
         return (acc += el);
       }, 0);
-      if(operator === "average"){
-        result /= numbers.length
+      if (operator === "average") {
+        result /= numbers.length;
       }
+    } else if (operator === "mode") {
+      result = {};
+      numbers.forEach((e) => {
+        if (result[e]) {
+          result[e] += 1;
+        } else {
+          result[e] = 1;
+        }
+      });
+      for (const k in result) {
+        // console.log(k, result[k])
+        if (modeOccurrence < result[k]) {
+          modeOccurrence = result[k];
+          modeNum = k;
+        }
+      }
+      result = modeNum;
+      console.log("modeNum", modeNum);
+      console.log("modeOccurrence", modeOccurrence);
     }
-
-    console.log(result);
+    props.changeResult(result);
+    console.log("result", result);
   };
 
   return (
