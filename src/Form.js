4bd51c773899c2f1e2bd079bcf43028c889f704b
NRiddick113 <NatashaRiddick@pursuit.org>
2022-10-13 01:00:55 -0400

    tring to fix mode

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,45 @@
 import React from "react";
+import { useState } from "react";
 import "./Form.css";
 
-function Form() {
+function Form({ setResult }) {
+  const [arr, setArr] = useState([])
+  const [select, setSelect] = useState('')
+
+  const addNumbers = (e) => {
+    setArr(e.target.value.split(",").map((e) => Number(e)))
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    setResult(numbers(arr, select))
+    if(!numbers(arr, select)){
+      return 'Invalid input.'
+    }
+   
+  }
+
+
+
+  const numbers = (option) => {
+    console.log(option)
+if (isNaN(option)) {
+  console.error('Invalid input.');
+}
+    let result = arr.reduce((a, b) =>{return (a+=b)}, 0)
+
+    if(select === "sum"){
+       return result
+    }if(select === "average"){
+       return result / arr.length
+    }else if (select === "mode"){
+      return Math.mode(arr)
+    }
+  }
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+    <form onSubmit={submitForm}>
+      <input id="values" name="values" type="text" onChange={addNumbers} value={arr}/>
+      <select id="operation" name="operation" value={select} onChange={(e) => {setSelect(e.target.value)}} >
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
