3ac8cb16cfceeb0fcc023e3d55dcb5cedd8c6fd7
TrianePeart <TrianePeart@gmail.com>
2022-10-12 21:11:57 -0400

    Stopping point

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React from 'react';
 import "./Form.css";
+import {useState} from 'react'
 
 function Form() {
+  const [values, setValues] = useState('')
+  const [select, setSelect] = useState('')
+
+  const handleChange = (e) => {
+    const { value } = e.target
+    setValues(value)
+  }
+
+  const handleSumbit = (e) => {
+    e.preventDefault()
+    // const values = e.target.values.value
+    
+    if (!select){
+      alert ("Invalid input")
+    } else return sum
+    //DONT LEAVE LIKE THIS ITS A TEST
+  }
+
+  function sum (values) {
+    return values.split(',').reduce((a, b) => Number(a + b))
+  }
+  //MAYBE MAKE A SWITCH STATEMENT?
+
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+    <form onSubmit={handleSumbit}>
+      <input id="values" name="values" type="text" onChange={handleChange} value={values}/>
+      <select id="operation" name="operation" onChange={(e) => {setSelect(e.target.value)}} >
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
