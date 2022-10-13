9fe84d498c32f4a3de49b6c0252dc11e8916bed7
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-10-12 20:59:10 -0400

    sum and average

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,38 @@
 import React from "react";
+import { useState } from "react";
 import "./Form.css";
 
 function Form() {
+  const [numbers, setNumbers] = useState([]);
+  const [operator, setOperator] = useState("");
+
+  const getNumbers = (e) => {
+    setNumbers(e.target.value.split(",").map((e) => Number(e)));
+  };
+
+  const calculate = (event) => {
+    event.preventDefault();
+    let result;
+    if (operator === "sum" || operator === "average") {
+      result = numbers.reduce((acc, el) => {
+        return (acc += el);
+      }, 0);
+      if(operator === "average"){
+        result /= numbers.length
+      }
+    }
+
+    console.log(result);
+  };
+
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+    <form onSubmit={calculate}>
+      <input id="values" name="values" type="text" onChange={getNumbers} />
+      <select
+        id="operation"
+        name="operation"
+        onChange={(e) => setOperator(e.target.value)}
+      >
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
