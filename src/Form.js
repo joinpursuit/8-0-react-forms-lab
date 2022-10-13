b71267b0d271662b903a3b6e90f306e230a6484c
Jonathan-Gaal <jonathangaal@pursuit.org>
2022-10-13 00:08:45 -0400

    math functionality working

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,95 @@
 import React from "react";
+import { useState } from "react";
 import "./Form.css";
 
-function Form() {
+function Form(props) {
+  //******** STATES *******/
+
+  // Textbox
+  // const [userTextBox, setUserTextBox] = useState([]);
+  const [numArr, setNumArr] = useState([]);
+
+  // Select Box
+  const [mySelect, setMySelect] = useState("");
+
+  //********** HELPERS *******/
+
+  //Errors
+  const errors = () => {
+    // typeof userTextBox !== "number" ? alert("invalid input") : null;
+  };
+
+  const convertToNumbers = (e) => {
+    const textBoxVals = e.target.value.split(",");
+    setNumArr(textBoxVals.map((e) => Number(e)));
+  };
+
+  const calculate = () => {
+    let sum;
+    let average;
+    let modeObj = {};
+    let modeOccurence = 0;
+    let modeNum;
+
+    let modeArr = [];
+    if (mySelect === "sum") {
+      // console.log(numArr);
+      sum = numArr.reduce((acc, num) => {
+        return (acc += num);
+      }, 0);
+      props.changeResult(sum);
+    }
+    if (mySelect === "average") {
+      sum = numArr.reduce((acc, num) => {
+        return (acc += num);
+      }, 0);
+      average = sum / numArr.length;
+      props.changeResult(average);
+    }
+
+    if (mySelect === "mode") {
+      modeObj = {};
+      numArr.forEach((e) => {
+        if (modeObj[e]) {
+          modeObj[e] += 1;
+        } else {
+          modeObj[e] = 1;
+        }
+      });
+      for (const key in modeObj) {
+        // console.log(modeObj);
+        if (modeOccurence < modeObj[key]) {
+          modeOccurence = modeObj[key];
+          modeNum = key;
+        }
+      }
+      props.changeResult(modeNum);
+    }
+  };
+
+  //Submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    calculate();
+    // console.log(sum);
+  };
+
+  //********* RETURN ********/
+
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+    <form onSubmit={handleSubmit}>
+      <input
+        onChange={convertToNumbers}
+        id="values"
+        name="values"
+        type="text"
+      />
+      <select
+        value={mySelect}
+        onChange={(e) => setMySelect(e.target.value)}
+        id="operation"
+        name="operation"
+      >
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
