77bc577d0c6d106b82ae43b821150ce0500d7ad6
NRiddick113 <NatashaRiddick@pursuit.org>
2022-10-13 02:35:24 -0400

    fixed mode

--- src/Form.js
+++ src/Form.js
@@ -20,21 +20,30 @@ function Form({setResult }) {
     }
   }
 
-
-
   const numbers = () => {
     let result = arr.reduce((a, b) =>{return (a+=b)}, 0)
-
     if(select === "sum"){
        return result
     }if(select === "average"){
        return result / arr.length
     }else if (select === "mode"){
-      return arr.find((mode) =>{
-
-      })
+     const obj = {}
+      let max = 0, count = 0;
+      for(let i = 0; i < arr.length; i++) {   
+      if(obj[arr[i]]) {
+        obj[arr[i]]++;
+      } else {
+        obj[arr[i]] = 1;
+      }
+      if(count < obj[arr[i]]) {
+        max = arr[i];
+        count = obj[arr[i]];
+      }
     }
+        return max
   }
+}
+
   return (
     <form onSubmit={submitForm}>
       <input id="values" name="values" type="text" onChange={addNumbers} value={arr}/>
