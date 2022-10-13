50a6f8dc8fe34a147c051039520e13b7db56e363
DestinyJoyner <destinyjoyner@pursuit.org>
2022-10-12 21:24:17 -0400

    cleaned up code,additional clarifying comments

--- src/Form.js
+++ src/Form.js
@@ -2,105 +2,108 @@ import React from "react";
 import { useState } from "react";
 import "./Form.css";
 
-
-function Form({result, setResult}) {
+function Form({ result, setResult }) {
   // Declare various slice of state
   // user input state
-  const [userInput, setUserInput] = useState('')
-  
+  const [userInput, setUserInput] = useState("");
+
   // select/option state
-  const [selectOption, setSelectOption] = useState("")
+  const [selectOption, setSelectOption] = useState("");
 
   // declare state for error class
-  const [error, setError] = useState(false)
+  const [error, setError] = useState(false);
 
-  // Declare variables for elements on page select/input
-  const select = document.querySelector(`select`)
-  const input = document.querySelector(`input`)
- 
   // Functions
-function handleUserInput(e) {
-  const value = e.target.value
-  setUserInput(value)
-}
+  // User input handler function
+  function handleUserInput(e) {
+    const value = e.target.value;
+    setUserInput(value);
+  }
 
-function convertUserInput(input, option) {
-  const arr = input.split(`,`)
-  let answer = 0;
-  let total = arr.reduce((num, acc )=> 
-     (+num) + (+acc)
-    ,answer)
-    
- // conditional for invalid input
-  if(isNaN(total) || input === `` || option === ``){
-    return 'Invalid input.'
+  // Convert User input from string to return number/ or 'invalid' function
+  function convertUserInput(input, option) {
+    const arr = input.split(`,`);
+    let answer = 0;
+    let total = arr.reduce((num, acc) => +num + +acc, answer);
+
+    // conditional for invalid input
+    if (isNaN(total) || input === `` || option === ``) {
+      return "Invalid input.";
     }
-    
-// conditionals for option choice
-  if(option === `sum`){
-   return total
-  }
-  if(option === `average`) {
-    const avg = total / arr.length
-    return avg
-  }
-  if(option === `mode`) {
-    const obj = {}
-    arr.map( num => {
-      if(!obj[num]){
-        obj[num] = 1
-      }
-      else {
-        obj[num] += 1
-      }
-    })
-    let mostNum = 0
-    let mostKey;
-    for ( let key in obj){
-      if(obj[key] > mostNum){
-        mostNum = obj[key]
-        mostKey = key
-      }
+
+    // conditionals for option choice
+    if (option === `sum`) {
+      return total;
     }
-    return mostKey
+    if (option === `average`) {
+      const avg = total / arr.length;
+      return avg;
+    }
+    if (option === `mode`) {
+      const obj = {};
+      arr.map((num) => {
+        if (!obj[num]) {
+          obj[num] = 1;
+        } else {
+          obj[num] += 1;
+        }
+      });
+      let mostNum = 0;
+      let mostKey;
+      for (let key in obj) {
+        if (obj[key] > mostNum) {
+          mostNum = obj[key];
+          mostKey = key;
+        }
+      }
+      return mostKey;
+    }
   }
-}
 
-// reset form input functions
-function reset() {
-  setUserInput(``)
-  setSelectOption(``)
-}
+  // reset form input functions
+  function reset() {
+    setUserInput(``);
+    setSelectOption(``);
+  }
 
+  //  Submit handler function
   function handleSubmit(e) {
-    e.preventDefault()
-    setResult(convertUserInput(userInput,selectOption))
-    if(convertUserInput(userInput,selectOption) !== `Invalid input.`){
-      setError(false)
-      reset()
+    e.preventDefault();
+    setResult(convertUserInput(userInput, selectOption));
+    if (convertUserInput(userInput, selectOption) !== `Invalid input.`) {
+      setError(false);
+      reset();
+    } else {
+      console.log(select, input);
+      setError(true);
     }
-    else{
-      console.log(select, input)
-      setError(true)
-    }
-    
   }
   return (
-    <form onSubmit={(event) => {handleSubmit(event)}}>
-      <input 
-      id="values" 
-      name="values" 
-      type="text"
-      value= {userInput} 
-      onChange={ (event) => {handleUserInput(event)} }
-      className= {error ? `error` : null}/>
+    <form
+      onSubmit={(event) => {
+        handleSubmit(event);
+      }}
+    >
+      <input
+        id="values"
+        name="values"
+        type="text"
+        value={userInput}
+        onChange={(event) => {
+          handleUserInput(event);
+        }}
+        className={error ? `error` : null}
+      />
 
-      <select 
-      id="operation" 
-      name="operation"
-      value={selectOption}
-      onChange={(event) => {setSelectOption(event.target.value)}}
-      className= {error ? `error` : null}>
+      <select
+        id="operation"
+        name="operation"
+        value={selectOption}
+        onChange={(event) => {
+          setSelectOption(event.target.value);
+        }}
+        className={error ? `error` : null}
+      >
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
