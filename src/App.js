b94cff1d421d20100bf97d3b0f0c363fee7942d6
Fadila-Ali <fadilaali@pursuit.org>
2022-10-13 18:03:10 -0400

    All Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 import "./App.css";
 
 function App() {
+  let [submitted, setSubmitted] = useState("");
+
   return (
     <main>
       <p>Enter each number in the array, separated by a ','</p>
-      <Form />
+      <Form setSubmitted={setSubmitted} />
       <section id="result">
-        <p></p>
+        <p>{submitted}</p>
       </section>
     </main>
   );
