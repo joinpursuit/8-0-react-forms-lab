f34eb29ae411f19b0694c4bb85fb6240f124e6df
bowersjuan <bowersjuan@gmail.com>
2022-10-14 03:42:56 -0400

    add state and input functions to form component

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,41 @@
 import React from "react";
+import { useState } from "react";
 import "./Form.css";
 
 function Form() {
+  const [inputStr, setInputStr] = useState("");
+  const [operation, setOperation] = useState("");
+
+  function handleInputChange(e) {
+    setInputStr(e.target.value);
+  }
+
+  function handleSelectChange(e) {
+    setOperation(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setInputStr("");
+    setOperation("");
+  }
+
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+    <form onSubmit={handleSubmit}>
+      <input
+        onChange={handleInputChange}
+        id="values"
+        name="values"
+        type="text"
+        value={inputStr}
+      />
+      <select
+        onChange={handleSelectChange}
+        id="operation"
+        name="operation"
+        value={operation}
+      >
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
