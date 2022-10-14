e86f208b99b20d95da5c6aa4da64a51fbdb8f935
Amina-Moufakkir <aminamoufakkir@pursuit.org>
2022-10-14 09:35:23 -0400

    react-forms-lab-completed

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,44 @@
 import React from "react";
+import { operations } from "./operations";
 import "./Form.css";
+import { useState } from "react";
 
-function Form() {
+function Form({ setResult }) {
+  const [error, setError] = useState(false);
+  const [numbers, setNumbers] = useState("");
+  const [operation, setOperation] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!numbers || !operation) {
+      setError(true);
+      return setResult("Invalid input.");
+    }
+    const result = operations[operation](numbers);
+    if (isNaN(result)) {
+      setError(true);
+      setResult("Invalid input.");
+    } else {
+      e.target.reset();
+      setError(false);
+      setResult(result);
+    }
+  };
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+    <form onSubmit={handleSubmit}>
+      <input
+        id="values"
+        name="values"
+        type="text"
+        className={error ? "error" : ""}
+        onChange={(e) => setNumbers(e.target.value)}
+      />
+      <select
+        id="operation"
+        name="operation"
+        className={error ? "error" : ""}
+        onChange={(e) => setOperation(e.target.value)}
+      >
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
---
+++ src/operations.js
@@ -0,0 +1,22 @@
+export const operations = {
+  sum: function (numbers) {
+    return numbers
+      .split(",")
+      .reduce((total, current) => (total += Number(current)), 0);
+  },
+  average: function (numbers) {
+    return this.sum(numbers) / numbers.split(",").length;
+  },
+  mode: function (numbers) {
+    numbers = numbers.replaceAll(",", "");
+    let mode = 0;
+    let occurence = 1;
+    numbers.split("").forEach((num) => {
+      if (numbers.split(num).length > occurence) {
+        mode = num;
+        occurence = numbers.split(num).length;
+      }
+    });
+    return mode;
+  },
+};
