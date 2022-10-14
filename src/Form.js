095164d77a4bc9d17739576dfa4bfce6e05b9b0b
D-Stukes <dstukes888@gmail.com>
2022-10-14 19:46:53 -0400

    added error state and class and invalid input messagefor bonus

--- src/Form.js
+++ src/Form.js
@@ -7,6 +7,7 @@ function Form({setResult}) {
   
  const [input, setInput] = useState("")
  const [selectOption, setSelectOption] = useState("")
+ const [error, setError] = useState(false)
 //  const collect = require('collect.js')
 
  const handleTextChange = (event) => {
@@ -20,7 +21,8 @@ function Form({setResult}) {
     let total = array.reduce((acc, currentArr) => {
         return Number(acc) + Number(currentArr)
       }, 0)
-    
+  if(isNaN(total ) || input === "" || selectOption === "")
+  return "Invalid input."
   if(selectOption === "sum"){
      return total
     // array.reduce((acc, currentArr) => {
@@ -43,25 +45,73 @@ function Form({setResult}) {
     // const collection = collect(array);
     // const mode_val = collection.mode();
     // console.log(mode_val);
-    console.log("MODE")
-   }
+    
+    const obj = {} 
+    //populating obj with key value pair
+    // alternative: obj.key 
+    // obj[key] = value
+    // obj.key = value
+     array.forEach(num => {
+      if(!obj[num]){
+        obj[num] = 1
+      }
+      else{
+        obj[num] += 1
+      }
+    })
+
+      //console.log(obj)
+  
+  //loop through object to check for the value that occur the most tiimes
+  let value = 0
+  let key; 
+  for (let thisKey in obj){
+    if(obj[thisKey] > value){
+      key = thisKey
+      value = obj[thisKey]
+    }
   }
+  return key
+  //console.log("key",key)
+  
+  }
+ }
+
+
+
+//********* HANDLE SUBMIT ********
+
 
  function handleSubmit(event){
   event.preventDefault()
   setResult(calculateResult(input))
-  // setInput('')
+if(calculateResult(input) !== "Invalid input."){
+  setError(false)
+  resetThisForm()
+} else {
+  setError(true)
+}
+
  }
+
+ function resetThisForm(){
+  setInput("")
+  setSelectOption("")
+ }
+
  
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
-      <input id="values" 
+      <input 
+             className = {error ? "error" : null}
+             id="values" 
              name="values" 
              type="text" 
              onChange={ (event) => handleTextChange(event)}
              value={input}
              />
       <select 
+        className = {error ? "error" : null}
         id="operation" 
         name="operation"
         value={selectOption}
