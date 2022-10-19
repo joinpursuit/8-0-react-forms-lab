0a669902a0bde7b69513b015231423d8b7630a9a
pursuit-monique <moniquecorrea@pursuit.orgname>
2022-10-18 21:52:50 -0400

    dang it.

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,54 @@
-import React from "react";
+import { useState } from "react";
 import "./Form.css";
 
-function Form() {
+function Form({ setResult }) {
+  const [operation, setOperation] = useState("");
+  const [numbers, setNumbers] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    function mode(arr) {
+      return arr
+        .sort(
+          (a, b) =>
+            arr.filter((v) => v === a).length -
+            arr.filter((v) => v === b).length
+        )
+        .pop();
+    }
+
+    let numArray = numbers.split(",").map((num) => Number(num));
+    let total;
+
+    if (!operation || !numbers) {
+      setResult("Invalid input.");
+    } else {
+      total = numArray.reduce((prev, curr) => prev + curr, 0);
+
+      if (operation === "sum") {
+        setResult(total);
+      } else if (operation === "average") {
+        setResult(total / numbers.split(",").length);
+      } else {
+        setResult(mode(numArray));
+      }
+    }
+  }
+
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+    <form onSubmit={handleSubmit}>
+      <input
+        id="values"
+        name="values"
+        type="text"
+        onChange={(event) => setNumbers(event.target.value)}
+      />
+      <select
+        id="operation"
+        name="operation"
+        onChange={(event) => setOperation(event.target.value)}
+      >
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
