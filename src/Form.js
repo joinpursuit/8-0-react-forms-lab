b9ebaf0721bedb67c65420dcd78be7969d01fa87
youssefmohamedfarouk <youssefmohamedfarouk@pursuit.org>
2022-10-18 20:26:37 -0400

    Add state to Form.js and App.js to calculate sum, average, and mode. Completed bonus tests as well for changing element classes on error.

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
-function Form() {
+function Form({ result, setResult }) {
+  const [textInput, setTextInput] = useState("");
+  const [select, setSelect] = useState("");
+  const [isBorken, setBorken] = useState("notBorken");
+
+  function submit(e) {
+    e.preventDefault();
+    if (/[^\d,]/.test(textInput) || !select) {
+      setBorken("error");
+      setResult("Invalid input.");
+    } else {
+      const numArr = textInput.split(",").map((a) => Number(a));
+      setTextInput('');
+      setSelect('');
+      setBorken("notBorken");
+      if (select === "sum") {
+        setResult(numArr.reduce((a, b) => a + b, 0));
+      } else if (select === "average") {
+        setResult(numArr.reduce((a, b) => a + b, 0) / numArr.length);
+      } else if (select === "mode") {
+        const store = {};
+        numArr.forEach((num) =>
+          store[num] ? (store[num] += 1) : (store[num] = 1)
+        );
+        setResult(Object.keys(store).sort((a, b) => store[b] - store[a])[0]);
+      }
+    }
+  }
+
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+    <form onSubmit={submit}>
+      <input
+        className={isBorken}
+        id="values"
+        name="values"
+        type="text"
+        value={textInput}
+        onChange={(e) => setTextInput(e.target.value)}
+      />
+      <select
+        className={isBorken}
+        id="operation"
+        name="operation"
+        value={select}
+        onChange={(e) => setSelect(e.target.value)}
+      >
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
