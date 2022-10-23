be554670478ca2e5a4b2b435a47b486fd6e099b8
bigdaddyjean <jeannappoleon@pursuit.org>
2022-10-22 20:51:21 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import Form from "./Form";
 import "./App.css";
 
+
 function App() {
+  const [result, setResult] = useState("");
   return (
     <main>
       <p>Enter each number in the array, separated by a ','</p>
-      <Form />
+      <Form setResult={setResult} />
       <section id="result">
-        <p></p>
+        <p>{result}</p>
       </section>
     </main>
   );
--- src/Form.js
+++ src/Form.js
@@ -1,17 +1,90 @@
 import React from "react";
 import "./Form.css";
 
-function Form() {
+function Form({ setResult }) {
+  const [value, setValue] = useState("");
+  const [operator, setOperator] = useState("");
+  const invalidMsg = "Invalid input.";
+  const [isError, setIsError] = useState(false);
+
+  const showError = () => {
+    setResult(invalidMsg);
+    setIsError(true);
+  };
+
+  const findMod = (array) => {
+    if (!array.length) return null;
+    let mod = array[0];
+    let modFreq = 1;
+    for (let i = 0; i < array.length - 1; i++) {
+      const item = array[i];
+      let frequency = 1;
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] === item) {
+          frequency++;
+        }
+      }
+      if (frequency > modFreq) {
+        mod = item;
+      }
+    }
+    return mod;
+  };
+
+  const calc = (e) => {
+    e.preventDefault();
+    setResult("");
+    setIsError(false);
+    const array = value.split(",");
+
+    if (!operator || !array || !array.length) return showError();
+
+    let result;
+    switch (operator) {
+      case "sum":
+        result = array.reduce((accu, curr) => +accu + +curr, 0);
+        break;
+      case "average":
+        result = array.reduce((accu, curr) => +accu + +curr, 0) / array.length;
+        break;
+      case "mode":
+        result = findMod(array);
+        break;
+      default:
+        return showError();
+    }
+    if (!result) return showError();
+    else setResult(result);
+
+    setValue("");
+    setOperator("");
+  };
+
   return (
     <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+      <input
+        id="values"
+        name="values"
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className={isError && "error"}
+      />
+      <select
+        id="operation"
+        name="operation"
+        value={operator}
+        className={isError && "error"}
+        onChange={(e) => setOperator(e.target.value)}
+      >
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
         <option value="mode">mode</option>
       </select>
-      <button type="submit">Calculate</button>
+      <button type="submit" onClick={calc}>
+        Calculate
+      </button>
     </form>
   );
 }
