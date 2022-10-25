744e28f88e89381d133f984734e85f6e06be06c5
DanielR2105 <rodriguez.daniel2105@gmail.com>
2022-10-25 12:31:54 -0400

    temporarily stuck

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import React from "react";
+import { useState } from "react";
 import Form from "./Form";
 import "./App.css";
 
 function App() {
+
+  const [selectOption, setSelectOption] = useState("")
+  const [numbers, setNumbers] = useState([])
+  const [inputTotal, setInputTotal] = useState("")
+
+  function handleSelectChange(event) {
+    setSelectOption(event.target.value)
+  }
+
+  function handleNumbersChange(event) {
+    setNumbers(event.target.value)
+    
+  }
+
+  function calculateForm (selectOption) {
+    if (selectOption === "average") {
+
+    }
+    if (selectOption === "mode") {
+      
+    }
+    if (selectOption === "sum") {
+      let total = 0
+      console.log(total)
+      setInputTotal(total)
+      console.log(inputTotal)
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    calculateForm(selectOption)
+  }
+
   return (
     <main>
       <p>Enter each number in the array, separated by a ','</p>
-      <Form />
+      <Form handleSelectChange={handleSelectChange} handleSubmit={handleSubmit} handleNumbersChange={handleNumbersChange}/>
       <section id="result">
-        <p></p>
+        <p>{inputTotal}</p>
       </section>
     </main>
   );
--- src/Form.js
+++ src/Form.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./Form.css";
 
-function Form() {
+function Form({handleSelectChange, handleSubmit, handleNumbersChange}) {
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+    <form onSubmit={handleSubmit}>
+      <input id="values" name="values" type="text" onChange={handleNumbersChange}/>
+      <select id="operation" name="operation" onChange={handleSelectChange}>
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
         <option value="mode">mode</option>
       </select>
-      <button type="submit">Calculate</button>
+      <button type="submit" >Calculate</button>
     </form>
   );
 }
